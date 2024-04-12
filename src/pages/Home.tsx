b0f7e3b595ff3aa88ed89b6b19aeeb4e1a2af201
alfredo-petri <alfredo_petri@hotmail.com>
2024-04-12 17:59:21 -0300

    update  design on Login Page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,24 @@
 import React from "react"
-import { Avatar, Box, Button, Grid } from "@mui/material"
+import { Avatar, Box, Button, Grid, IconButton, Typography } from "@mui/material"
 import { Login } from "../components/login/Login"
 import destaque from "../assets/login/Frame747.svg"
 import logoMarca from "../assets/login/logo_wide.svg"
 import logo from "../assets/login/Vector.svg"
-
+import WhatsAppIcon from "@mui/icons-material/WhatsApp"
+import InstagramIcon from "@mui/icons-material/Instagram"
+import { FaFacebookF } from "react-icons/fa"
+import backGround from "../assets/login/background.gif"
 interface HomeProps {}
 
 export const Home: React.FC<HomeProps> = ({}) => {
     return (
-        <Box sx={{ width: 1, height: 1, backgroundColor: "primary.main" }}>
+        <Box sx={{ width: 1, height: "100vh" }}>
+            <Avatar
+                variant="square"
+                src={backGround}
+                sx={{ width: "100vw", height: "100vh", position: "absolute", top: 0, left: 0, objectFit: "fill", zIndex: -1 }}
+            />
+
             <Box
                 sx={{
                     width: "75vw",
@@ -18,21 +27,35 @@ export const Home: React.FC<HomeProps> = ({}) => {
                     padding: "5vw 0",
                     display: "flex",
                     flexDirection: "column",
-                    gap: "5vw",
+                    justifyContent: "space-between",
                 }}
             >
                 <Box justifyContent="space-between" gap="1.5vw">
                     <Box flex={0.2}>
                         <Avatar variant="square" src={logoMarca} sx={{ width: 1, height: 1, objectFit: "contain" }} />
                     </Box>
-                    <Box flex={0.7} sx={{justifyContent:"end", gap:"0.8vw"}}>
-                        <Button variant="text" color="secondary" sx={{ fontSize: "1.2rem", }}>Inicio</Button>
-                        <Button variant="text" color="secondary" sx={{ fontSize: "1.2rem", }}>Produtos</Button>
-                        <Button variant="text" color="secondary" sx={{ fontSize: "1.2rem", }}>Sobre</Button>
-                        <Button variant="text" color="secondary" sx={{ fontSize: "1.2rem", }}>Portfolio</Button>
-                        <Button variant="text" color="secondary" sx={{ fontSize: "1.2rem", }}>Academia</Button>
-                        <Button variant="text" color="secondary" sx={{ fontSize: "1.2rem", }}>Contato</Button>
-                        <Button variant="text" color="secondary" sx={{ fontSize: "1.2rem", }}>Comprar</Button>
+                    <Box flex={0.7} sx={{ justifyContent: "end", gap: "1.8vw" }}>
+                        <Button variant="text" color="secondary" sx={{ fontSize: "1.2rem" }}>
+                            Inicio
+                        </Button>
+                        <Button variant="text" color="secondary" sx={{ fontSize: "1.2rem" }}>
+                            Produtos
+                        </Button>
+                        <Button variant="text" color="secondary" sx={{ fontSize: "1.2rem" }}>
+                            Sobre
+                        </Button>
+                        <Button variant="text" color="secondary" sx={{ fontSize: "1.2rem" }}>
+                            Portfolio
+                        </Button>
+                        <Button variant="text" color="secondary" sx={{ fontSize: "1.2rem" }}>
+                            Academia
+                        </Button>
+                        <Button variant="text" color="secondary" sx={{ fontSize: "1.2rem" }}>
+                            Contato
+                        </Button>
+                        <Button variant="text" color="secondary" sx={{ fontSize: "1.2rem" }}>
+                            Comprar
+                        </Button>
                     </Box>
                 </Box>
 
@@ -44,6 +67,23 @@ export const Home: React.FC<HomeProps> = ({}) => {
                         <Login />
                     </Grid>
                 </Grid>
+                <Box sx={{ justifyContent: "space-between" }}>
+                    <Box gap="1.5vw">
+                        <IconButton sx={{ backgroundColor: "secondary.main", color: "primary.main" }}>
+                            <WhatsAppIcon sx={{ fontSize: "1.2rem" }} />
+                        </IconButton>
+                        <IconButton sx={{ backgroundColor: "secondary.main", color: "primary.main" }}>
+                            <InstagramIcon sx={{ fontSize: "1.2rem" }} />
+                        </IconButton>
+                        <IconButton sx={{ backgroundColor: "secondary.main", color: "primary.main" }}>
+                            <FaFacebookF style={{ fontSize: "1.2rem" }} />
+                        </IconButton>
+                    </Box>
+                    <Box gap="1vw" sx={{ color: "secondary.main" }}>
+                        <Avatar src={logo} sx={{ height: "1.5rem", width: "1.5rem" }} />
+                        <Typography variant="body1">© 2024 Loucas & Lisas</Typography>
+                    </Box>
+                </Box>
             </Box>
         </Box>
     )
