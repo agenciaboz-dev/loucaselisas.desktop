b9b3c2c31a326a92c26d241b1a12f5f3317c4871
alfredo-petri <alfredo_petri@hotmail.com>
2024-04-15 18:33:47 -0300

    create DashBoard

---
+++ src/pages/DashBoard.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+import { Box } from "@mui/material"
+import { MenuPrincipal } from "../components/dashboard/Menu"
+
+interface DashBoardProps {}
+
+export const DashBoard: React.FC<DashBoardProps> = ({}) => {
+    return <Box sx={{ width: 1, height: 1 }}>Conteudo da dashboard</Box>
+}
