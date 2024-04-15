8542be557d777f0bed69ef8ea16faebee480b64c
alfredo-petri <alfredo_petri@hotmail.com>
2024-04-15 18:32:49 -0300

    create Menu

---
+++ src/components/dashboard/Menu.tsx
@@ -0,0 +1,57 @@
+import React from "react"
+import { Avatar, Box, Drawer, List, ListItemButton, ListItemIcon, ListItemText, useTheme } from "@mui/material"
+import AssignmentTurnedInIcon from "@mui/icons-material/AssignmentTurnedIn"
+import AutoStoriesIcon from "@mui/icons-material/AutoStories"
+import GroupIcon from "@mui/icons-material/Group"
+import GroupsIcon from "@mui/icons-material/Groups"
+import PersonalVideoIcon from "@mui/icons-material/PersonalVideo"
+import ViewListIcon from "@mui/icons-material/ViewList"
+import SettingsIcon from "@mui/icons-material/Settings"
+import { useNavigate } from "react-router-dom"
+
+interface IMenuOptions {
+    label: string
+    icon: React.ReactNode
+    path: string
+}
+
+interface MenuPrincipalProps {}
+
+export const MenuPrincipal: React.FC<MenuPrincipalProps> = ({}) => {
+    const menuOptions: IMenuOptions[] = [
+        { label: "Painel", icon: <ViewListIcon />, path: "/dashboard" },
+        { label: "Cursos", icon: <PersonalVideoIcon />, path: "/courses" },
+        { label: "Lições", icon: <AutoStoriesIcon />, path: "/lessons" },
+        { label: "Aprovar", icon: <AssignmentTurnedInIcon />, path: "/check" },
+        { label: "Grupos", icon: <GroupsIcon />, path: "/groups" },
+        { label: "Usuários", icon: <GroupIcon />, path: "/users" },
+    ]
+
+    const navigate = useNavigate()
+
+    const theme = useTheme()
+
+    return (
+        <Box sx={{}}>
+            <Drawer variant="permanent" sx={{ width: theme.spacing(20), padding: "2vw", position: "relative", top: "4vw", left: "4vw" }}>
+                <Avatar variant="circular" />
+                <List sx={{ flex: 1 }}>
+                    {menuOptions.map((menuOption) => (
+                        <ListItemButton key={menuOption.path} onClick={() => navigate(menuOption.path)}>
+                            <ListItemIcon>{menuOption.icon}</ListItemIcon>
+                            <ListItemText>{menuOption.label}</ListItemText>
+                        </ListItemButton>
+                    ))}
+                </List>
+                <List sx={{ mb: "1vw" }}>
+                    <ListItemButton onClick={() => navigate("/settings")}>
+                        <ListItemIcon>
+                            <SettingsIcon />
+                        </ListItemIcon>
+                        <ListItemText>Configurações</ListItemText>
+                    </ListItemButton>
+                </List>
+            </Drawer>
+        </Box>
+    )
+}
