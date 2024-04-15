57c539cc0a20a619b3e09fda76dbeaa484c64f00
alfredo-petri <alfredo_petri@hotmail.com>
2024-04-15 12:51:55 -0300

    update background

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,18 +1,24 @@
-import React from 'react'
-import {Box} from '@mui/material'
-import { Routes as ReactRoutes, Route } from 'react-router-dom'
-import { Home } from './pages/Home'
+import React from "react"
+import { Box, Card } from "@mui/material"
+import { Routes as ReactRoutes, Route } from "react-router-dom"
+import { Home } from "./pages/Home"
+import backgroundVideo from "./assets/background.webm"
 
-interface RoutesProps {
-    
-}
+interface RoutesProps {}
 
-export const Routes:React.FC<RoutesProps> = ({  }) => {
-    
+export const Routes: React.FC<RoutesProps> = ({}) => {
     return (
         <Box>
+            <video
+                autoPlay
+                loop
+                muted
+                style={{ pointerEvents: "none", zIndex: -1, position: "absolute", width: "100vw", height: "100vh", objectFit: "cover" }}
+                src={backgroundVideo}
+            ></video>
+
             <ReactRoutes>
-                <Route path='/' element={<Home/>} />
+                <Route path="/" element={<Home />} />
             </ReactRoutes>
         </Box>
     )
