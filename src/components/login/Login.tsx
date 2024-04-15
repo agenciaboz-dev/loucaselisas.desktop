f9a0735d752bacd5e6c86cf87d3285b1040d9f41
alfredo-petri <alfredo_petri@hotmail.com>
2024-04-15 14:55:47 -0300

    feat login

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,45 +1,70 @@
-import React, { useState } from 'react'
-import {Box, Button, CircularProgress, IconButton, TextField, Typography} from '@mui/material'
-import { Form } from './Form'
+import React, { useState } from "react"
+import { Box, Button, CircularProgress, IconButton, TextField, Typography } from "@mui/material"
+import { Form } from "./Form"
 import { useFormik } from "formik"
-import { LoginForm } from '../../types/server/login'
+import { LoginForm } from "../../types/server/login"
 
 import VisibilityIcon from "@mui/icons-material/Visibility"
 import VisibilityOffIcon from "@mui/icons-material/VisibilityOff"
 import PersonIcon from "@mui/icons-material/Person"
 import KeyIcon from "@mui/icons-material/Key"
+import { api } from "../../api/api"
+import { User } from "../../types/server/class"
+import { useUser } from "../../hooks/useUser"
 
-interface LoginProps {
-    
-}
+interface LoginProps {}
 
-export const Login: React.FC<LoginProps> = ({ }) => {
-    
+export const Login: React.FC<LoginProps> = ({}) => {
+    const { onLogin } = useUser()
+    const [errorLogin, setErrorLogin] = useState(false)
     const formik = useFormik<LoginForm>({
         initialValues: {
             login: "",
             password: "",
         },
-        onSubmit: (values) => {
+        onSubmit: async (values) => {
             console.log(values)
+
+            if (loading) return
+
+            setLoading(true)
+            const data: LoginForm = {
+                login: values.login,
+                password: values.password,
+            }
+
+            try {
+                const response = await api.post("/login/admin", data)
+                console.log(response.data)
+                const user = response.data as User | null
+
+                if (!user) {
+                    setErrorLogin(true)
+                    return
+                }
+
+                onLogin(user)
+            } catch (error) {
+                console.error("Erro ao buscar os dados:", error)
+            } finally {
+                setLoading(false)
+            }
         },
     })
 
     const [showPassword, setShowPassword] = useState(false)
     const [loading, setLoading] = useState(false)
 
-    
     return (
-        
-            <Box sx={{ height: 1, width: 1, padding:"1.5vw", gap: "2vw", flexDirection: "column", backgroundColor: "background.default" }}>
-            <Typography variant="h1" component="h1" sx={{fontSize:"3rem"}}>
+        <Box sx={{ height: 1, width: 1, padding: "1.5vw", gap: "2vw", flexDirection: "column", backgroundColor: "background.default" }}>
+            <Typography variant="h1" component="h1" sx={{ fontSize: "3rem" }}>
                 Administrativo
             </Typography>
             <Typography variant="body1" component="p">
                 Esta área é exclusiva para administradores.
             </Typography>
             <Box sx={{ gap: "2vw", flexDirection: "column" }}>
-                <Form onSubmit={formik.handleSubmit} sx={{ height: 1, flexDirection: "column", gap:"2vw"}}>
+                <Form onSubmit={formik.handleSubmit} sx={{ height: 1, flexDirection: "column", gap: "2vw" }}>
                     <TextField
                         label="Usuário ou Email"
                         variant="standard"
@@ -72,15 +97,14 @@ export const Login: React.FC<LoginProps> = ({ }) => {
                             ),
                         }}
                     />
-                    <Box sx={{justifyContent:"end"}}>
-
-                    <Button type="submit" variant="contained" sx={{borderRadius:0, width: "30%"}}>
-                        {loading ? <CircularProgress size={"1.5rem"} color="inherit" /> : "Entrar"}
-                    </Button>
+                    {errorLogin && <Typography color="error.main">Usuário ou senha incorretos</Typography>}
+                    <Box sx={{ justifyContent: "end" }}>
+                        <Button type="submit" variant="contained" sx={{ borderRadius: 0, width: "30%" }}>
+                            {loading ? <CircularProgress size={"1.5rem"} color="inherit" /> : "Entrar"}
+                        </Button>
                     </Box>
                 </Form>
             </Box>
         </Box>
-        
     )
 }
