e54f7d04e04b9bbf84410e99a687ed77907b28ca
alfredo-petri <alfredo_petri@hotmail.com>
2024-04-16 18:06:48 -0300

    update DashBoard

--- src/pages/DashBoard.tsx
+++ src/pages/DashBoard.tsx
@@ -1,9 +1,41 @@
-import React from "react"
+import React, { useEffect, useRef, useState } from "react"
 import { Box } from "@mui/material"
-import { MenuPrincipal } from "../components/dashboard/Menu"
+import { api } from "../api/api"
+import { CoursersThumb } from "../components/dashboard/CoursesThumb"
+import { Course } from "../types/server/class/Course"
+import { useDraggable } from "react-use-draggable-scroll"
 
 interface DashBoardProps {}
 
 export const DashBoard: React.FC<DashBoardProps> = ({}) => {
-    return <Box sx={{ width: 1, height: 1 }}>Conteudo da dashboard</Box>
+    const [courses, setCourses] = useState<Course[]>([])
+
+    const getCourses = async () => {
+        try {
+            const response = await api.get("/course/all")
+
+            setCourses(response.data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        getCourses()
+    }, [])
+
+    console.log(courses)
+
+    const ref = useRef<HTMLElement>() as React.MutableRefObject<HTMLInputElement>
+    const { events } = useDraggable(ref, { applyRubberBandEffect: true })
+
+    return (
+        <Box>
+            <Box ref={ref} {...events} sx={{ width: "73.5vw", overflowX: "scroll", height: 1, scrollbarWidth: "none", pointerEvents: "auto" }}>
+                {courses.map((course) => (
+                    <CoursersThumb key={course.id} course={course} />
+                ))}
+            </Box>
+        </Box>
+    )
 }
