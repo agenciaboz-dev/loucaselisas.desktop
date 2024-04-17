3ec3957bae060bc4e78370388ba309a768ba3595
alfredo-petri <alfredo_petri@hotmail.com>
2024-04-17 17:57:39 -0300

    update CourseThumb

--- src/components/dashboard/CoursesThumb.tsx
+++ src/components/dashboard/CoursesThumb.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Avatar, Box, Typography } from "@mui/material"
+import { Avatar, Box, Paper, Typography } from "@mui/material"
 import { Course } from "../../types/server/class/Course"
 
 interface CoursersThumbProps {
@@ -7,26 +7,27 @@ interface CoursersThumbProps {
 }
 
 export const CoursersThumb: React.FC<CoursersThumbProps> = ({ course }) => {
-    const numAulas = course.lessons.length.toString().padStart(2, "0")
+    const numAulas = course.lessons.length?.toString().padStart(2, "0") || 9999
     return (
-        <Box
+        <Paper
+            elevation={2}
             sx={{
                 height: "13vw",
                 aspectRatio: "4/3",
                 flexDirection: "column",
-                mr: "0.5vw",
                 borderRadius: "1.2vw",
-                border: "1px solid red",
                 overflow: "hidden",
                 position: "relative",
             }}
         >
             <Avatar
-                src={course.cover}
+                src={course?.cover}
                 alt="imagem de capa do curso"
                 variant="square"
                 sx={{ objectFit: "cover", width: 1, height: 1, pointerEvents: "none" }}
-            />
+            >
+                <Avatar sx={{ width: "8vw", height: "8vw" }} variant="circular" src="./placeholders/midia_1-1.webp" />
+            </Avatar>
             <Box
                 sx={{
                     flexDirection: "column",
@@ -44,6 +45,6 @@ export const CoursersThumb: React.FC<CoursersThumbProps> = ({ course }) => {
                 <Typography>{course.name}</Typography>
                 <Typography>{numAulas} aulas</Typography>
             </Box>
-        </Box>
+        </Paper>
     )
 }
