c30a4d3cb5b7f1de3a65df3e02fea94b0d255d85
alfredo-petri <alfredo_petri@hotmail.com>
2024-04-18 16:13:39 -0300

    create Aprove

---
+++ src/pages/Aprove.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+import { Box } from "@mui/material"
+
+interface AproveProps {}
+
+export const Aprove: React.FC<AproveProps> = ({}) => {
+    return <Box sx={{}}>Aprovar</Box>
+}
