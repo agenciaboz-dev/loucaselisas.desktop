9a6a7d391ae0dd0c8547a43de712d419acb95cad
alfredo-petri <alfredo_petri@hotmail.com>
2024-04-18 16:31:20 -0300

    create Settings

---
+++ src/pages/Settings.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+import { Box } from "@mui/material"
+
+interface SettingsProps {}
+
+export const Settings: React.FC<SettingsProps> = ({}) => {
+    return <Box sx={{}}>Configurações</Box>
+}
