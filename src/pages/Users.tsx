b07969209008c3277a194983b0008bc4bbb39696
alfredo-petri <alfredo_petri@hotmail.com>
2024-04-18 16:14:31 -0300

    create Users

---
+++ src/pages/Users.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+import { Box } from "@mui/material"
+
+interface UsersProps {}
+
+export const Users: React.FC<UsersProps> = ({}) => {
+    return <Box sx={{}}>Usuarios</Box>
+}
