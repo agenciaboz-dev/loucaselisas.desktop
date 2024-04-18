b7782b26516e4db4ab02e260e22f91658485b5e0
alfredo-petri <alfredo_petri@hotmail.com>
2024-04-18 16:13:57 -0300

    create Courses

---
+++ src/pages/Courses.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+import { Box } from "@mui/material"
+
+interface CourrsesProps {}
+
+export const Courrses: React.FC<CourrsesProps> = ({}) => {
+    return <Box sx={{}}>Cursos</Box>
+}
