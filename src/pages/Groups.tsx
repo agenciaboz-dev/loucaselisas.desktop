fb51fb2da40b13fc600abffce0e55979b93e9f94
alfredo-petri <alfredo_petri@hotmail.com>
2024-04-18 16:14:07 -0300

    create Groups

---
+++ src/pages/Groups.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+import { Box } from "@mui/material"
+
+interface GroupsProps {}
+
+export const Groups: React.FC<GroupsProps> = ({}) => {
+    return <Box sx={{}}>Grupos</Box>
+}
