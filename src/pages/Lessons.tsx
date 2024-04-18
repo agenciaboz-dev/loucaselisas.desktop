052450443ee3833344a7ebb0af5240a40417e3d3
alfredo-petri <alfredo_petri@hotmail.com>
2024-04-18 16:14:21 -0300

    create Lessons

---
+++ src/pages/Lessons.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+import { Box } from "@mui/material"
+
+interface LessonsProps {}
+
+export const Lessons: React.FC<LessonsProps> = ({}) => {
+    return <Box sx={{}}>Lições</Box>
+}
