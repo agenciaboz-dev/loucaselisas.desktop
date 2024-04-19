bbbbbb06b185b89131bbff31f5f656a210c7f501
alfredo-petri <alfredo_petri@hotmail.com>
2024-04-19 17:47:53 -0300

    update DashBoard

--- src/pages/DashBoard.tsx
+++ src/pages/DashBoard.tsx
@@ -5,6 +5,7 @@ import { Course } from "../types/server/class/Course"
 import { useDraggable } from "react-use-draggable-scroll"
 import { CoursesList } from "../components/dashboard/CoursesList"
 import { useGetCourses } from "../hooks/useGetCourses"
+import { HeaderInfo } from "../components/header/HeaderInfo"
 
 interface DashBoardProps {
     setRefreshCallback: React.Dispatch<React.SetStateAction<() => void>>
@@ -48,64 +49,67 @@ export const DashBoard: React.FC<DashBoardProps> = ({ setRefreshCallback, setCar
     const { events } = useDraggable(ref, { applyRubberBandEffect: true })
 
     return (
-        <Box
-            sx={{
-                flexDirection: "column",
-                height: "64vh",
-                width: 1,
-                gap: "0.8vw",
-            }}
-        >
-            <Box ref={ref} {...events} sx={{ width: "73.5vw", overflowX: "scroll", height: "auto", scrollbarWidth: "none", gap: "0.5vw" }}>
-                {loading
-                    ? skeletonCourse.map((course) => (
-                          <Skeleton
-                              key={course.index}
-                              variant="rounded"
-                              animation="wave"
-                              sx={{
-                                  height: "13vw",
-                                  aspectRatio: "4/3",
-                                  flexShrink: 0,
-                                  borderRadius: "1.2vw",
-                              }}
-                          />
-                      ))
-                    : courses.map((course) => <CoursersThumb key={course.id} course={course} />)}
-            </Box>
-
-            <Typography variant="h2" component="h2" sx={{ fontSize: "1.2rem" }}>
-                Explorar
-            </Typography>
+        <>
+            <HeaderInfo title="painel de controle" loading={false} refreshCallback={() => {}} />
             <Box
                 sx={{
-                    overflowY: "scroll",
-                    flex: 1,
                     flexDirection: "column",
-                    // height: "55vh",
-                    gap: "0.5vw",
-                    border: "1px sold blue",
-                    pt: "0.1vw",
-                    mx: "-1.5vw",
-                    px: "1.5vw",
+                    height: "64vh",
+                    width: 1,
+                    gap: "0.8vw",
                 }}
             >
-                <Grid container columns={2} spacing={2}>
+                <Box ref={ref} {...events} sx={{ width: "73.5vw", overflowX: "scroll", height: "auto", scrollbarWidth: "none", gap: "0.5vw" }}>
                     {loading
                         ? skeletonCourse.map((course) => (
-                              <Grid item key={course.index} xs={1}>
-                                  <Paper elevation={3} sx={{ flex: 1, p: "0.5vw", gap: "1vw" }}>
-                                      <Skeleton variant="rounded" animation="wave" sx={{ width: "5vw", height: "5vw" }} />
-                                      <Box sx={{ justifyContent: "space-between", flexDirection: "column" }}>
-                                          <Skeleton variant="text" animation="wave" sx={{ width: "10vw" }} />
-                                          <Skeleton variant="text" animation="wave" sx={{ width: "28.5vw", height: "4vw" }} />
-                                      </Box>
-                                  </Paper>
-                              </Grid>
+                              <Skeleton
+                                  key={course.index}
+                                  variant="rounded"
+                                  animation="wave"
+                                  sx={{
+                                      height: "13vw",
+                                      aspectRatio: "4/3",
+                                      flexShrink: 0,
+                                      borderRadius: "1.2vw",
+                                  }}
+                              />
                           ))
-                        : courses.map((course) => <CoursesList key={course.id} course={course} />)}
-                </Grid>
+                        : courses.map((course) => <CoursersThumb key={course.id} course={course} />)}
+                </Box>
+
+                <Typography variant="h2" component="h2" sx={{ fontSize: "1.2rem" }}>
+                    Explorar
+                </Typography>
+                <Box
+                    sx={{
+                        overflowY: "scroll",
+                        flex: 1,
+                        flexDirection: "column",
+                        // height: "55vh",
+                        gap: "0.5vw",
+                        border: "1px sold blue",
+                        pt: "0.1vw",
+                        mx: "-1.5vw",
+                        px: "1.5vw",
+                    }}
+                >
+                    <Grid container columns={2} spacing={2}>
+                        {loading
+                            ? skeletonCourse.map((course) => (
+                                  <Grid item key={course.index} xs={1}>
+                                      <Paper elevation={3} sx={{ flex: 1, p: "0.5vw", gap: "1vw" }}>
+                                          <Skeleton variant="rounded" animation="wave" sx={{ width: "5vw", height: "5vw" }} />
+                                          <Box sx={{ justifyContent: "space-between", flexDirection: "column" }}>
+                                              <Skeleton variant="text" animation="wave" sx={{ width: "10vw" }} />
+                                              <Skeleton variant="text" animation="wave" sx={{ width: "28.5vw", height: "4vw" }} />
+                                          </Box>
+                                      </Paper>
+                                  </Grid>
+                              ))
+                            : courses.map((course) => <CoursesList key={course.id} course={course} />)}
+                    </Grid>
+                </Box>
             </Box>
-        </Box>
+        </>
     )
 }
