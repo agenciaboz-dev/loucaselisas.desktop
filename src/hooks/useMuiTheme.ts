15f8d2e73efd927a9f83b28826b5425754e9dca5
alfredo-petri <alfredo_petri@hotmail.com>
2024-04-19 15:12:21 -0300

    update Theme

--- src/hooks/useMuiTheme.ts
+++ src/hooks/useMuiTheme.ts
@@ -21,21 +21,53 @@ export const usemuiTheme = () => {
             },
         },
         typography: {
-            fontFamily: ["FoundersGroteskCondensed"].join(","),
-            fontSize: 14,
+            fontFamily: ["FoundersGrotesk", "sans-serif"].join(","),
+            fontSize: 16,
+
             h1: {
-                fontWeight: 900,
+                fontFamily: "FoundersGroteskCondensed",
+                fontWeight: 300,
+                fontSize: "1.8rem",
             },
             h2: {
-                fontWeight: 700,
+                fontFamily: "FoundersGroteskCondensed",
+                fontWeight: 300,
+                fontSize: "1.5rem",
             },
-            body1: {
-                fontWeight: 400,
+            h3: {
+                fontFamily: "FoundersGroteskXCondensed",
+                fontWeight: 700,
+                fontSize: "1.3rem",
+            },
+            h4: {
+                fontFamily: "FoundersGroteskXCondensed",
+                fontWeight: 300,
+                fontSize: "1.1rem",
             },
             subtitle1: {
-                fontSize: 500,
+                fontFamily: "FoundersGrotesk",
+                fontWeight: 700,
+                fontSize: "1rem",
+            },
+            body1: {
+                fontFamily: "FoundersGrotesk",
+                fontWeight: 400,
+                fontSize: "0.9rem",
+            },
+            body2: {
+                fontFamily: "FoundersGrotesk",
+                fontWeight: 400,
             },
         },
     })
     return THEME
 }
+
+//titulo login bold 5.2rem
+//titulo principal h1 xcondensed lt 1.8rem
+//menu item h2 condensed lt - 1.5rem
+//username h3 xcondensed bold - 1.3rem 
+//função h4 xcondensed - 1.1 rem
+//subtitle bold - 1rem
+//body 1 regular - 0.9 rem
+//body 2 regular - variavel
