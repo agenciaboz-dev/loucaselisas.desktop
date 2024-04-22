4bdc19ae35dbb1a45c078706bffa9711619eb1fc
alfredo-petri <alfredo_petri@hotmail.com>
2024-04-22 17:48:51 -0300

    update DashBoard

--- src/pages/DashBoard.tsx
+++ src/pages/DashBoard.tsx
@@ -1,15 +1,13 @@
 import React, { useEffect, useState } from "react"
-import { Box, Grid, Paper, Skeleton, Typography } from "@mui/material"
+import { Box, Typography } from "@mui/material"
 import { Course } from "../types/server/class/Course"
-import { CoursesList } from "../components/dashboard/CoursesList"
 import { useGetCourses } from "../hooks/useGetCourses"
 import { HeaderInfo } from "../components/header/HeaderInfo"
-import { SearchBar } from "../components/header/SearchBar"
 import { FilteredCourses } from "../components/dashboard/FilteredCourses"
+import { FilteredListCourses } from "../components/dashboard/FilteredListCourses"
 
 export const DashBoard: React.FC = ({}) => {
     const [courses, setCourses] = useState<Course[]>([])
-    const skeletonCourse = new Array(20).fill(`course`)
     const { getCourses, loading } = useGetCourses()
 
     const fetchCourses = async () => {
@@ -33,48 +31,20 @@ export const DashBoard: React.FC = ({}) => {
     return (
         <Box sx={{ width: 1, flexDirection: "column" }}>
             <HeaderInfo title="painel de controle" loading={loading} refreshCallback={() => fetchCourses()} />
-            <SearchBar />
             <Box
                 sx={{
                     flexDirection: "column",
-                    height: "64vh",
+                    height: "72.8vh",
                     width: 1,
                     gap: "0.8vw",
-                    pt: "1vw",
+                    pt: "0.2vw",
                 }}
             >
                 <FilteredCourses loading={loading} courses={courses} />
-
                 <Typography variant="h2" component="h2" sx={{ fontSize: "1.2rem" }}>
                     Explorar
                 </Typography>
-                <Box
-                    sx={{
-                        overflowY: "scroll",
-                        flex: 1,
-                        flexDirection: "column",
-                        gap: "0.5vw",
-                        pt: "0.1vw",
-                        mx: "-1.5vw",
-                        px: "1.5vw",
-                    }}
-                >
-                    <Grid container columns={2} spacing={2}>
-                        {loading
-                            ? skeletonCourse.map((_, index) => (
-                                  <Grid item key={index} xs={1}>
-                                      <Paper elevation={3} sx={{ flex: 1, p: "0.5vw", gap: "1vw" }}>
-                                          <Skeleton variant="rounded" animation="wave" sx={{ width: "5vw", height: "5vw" }} />
-                                          <Box sx={{ justifyContent: "space-between", flexDirection: "column" }}>
-                                              <Skeleton variant="text" animation="wave" sx={{ width: "10vw" }} />
-                                              <Skeleton variant="text" animation="wave" sx={{ width: "28.5vw", height: "4vw" }} />
-                                          </Box>
-                                      </Paper>
-                                  </Grid>
-                              ))
-                            : courses.map((course) => <CoursesList key={course.id} course={course} />)}
-                    </Grid>
-                </Box>
+                <FilteredListCourses courses={courses} loading={loading} />
             </Box>
         </Box>
     )
