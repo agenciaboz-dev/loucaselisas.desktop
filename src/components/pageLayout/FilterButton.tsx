9862128baef5741921df7373e1f1e60c1ef37590
alfredo-petri <alfredo_petri@hotmail.com>
2024-04-22 15:23:53 -0300

    create Filter Button

---
+++ src/components/pageLayout/FilterButton.tsx
@@ -0,0 +1,25 @@
+import { Button } from "@mui/material"
+import React from "react"
+
+interface FilterButtonProps {
+    active?: boolean
+    content: string
+    onClickCategory: () => void
+}
+
+export const FilterButton: React.FC<FilterButtonProps> = ({ active, content, onClickCategory }) => {
+    return (
+        <>
+            <Button
+                variant={active ? "contained" : "outlined"}
+                onClick={() => onClickCategory()}
+                sx={{
+                    width: "10vw",
+                    borderRadius: "2vw",
+                }}
+            >
+                {content}
+            </Button>
+        </>
+    )
+}
