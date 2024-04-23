187df7c3f09f4b501a9e682bd7f795bceec33c4c
alfredo-petri <alfredo_petri@hotmail.com>
2024-04-23 17:54:35 -0300

    feat connection with back end

--- src/pages/Lessons.tsx
+++ src/pages/Lessons.tsx
@@ -1,8 +1,31 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { Box } from "@mui/material"
+import { Lesson } from "../types/server/class/Course/Lesson"
+import { api } from "../api/api"
 
 interface LessonsProps {}
 
 export const Lessons: React.FC<LessonsProps> = ({}) => {
-    return <Box sx={{}}>Lições</Box>
+    const [loading, setLoading] = useState(false)
+
+    const getLessons = async () => {
+        let lessons: Lesson[] | undefined
+        setLoading(true)
+
+        try {
+            const response = await api.get("lesson/all")
+            lessons = response.data
+            console.log({ lessons: response.data })
+        } catch (error) {
+            console.error(error)
+        } finally {
+            setTimeout(() => setLoading(false), 500)
+        }
+    }
+
+    useEffect(() => {
+        getLessons()
+    }, [])
+
+    return <Box sx={{ color: loading ? "red" : "" }}>Lições</Box>
 }
