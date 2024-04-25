2fa9007bfc1679976888a3ea1616c3077d8167e9
alfredo-petri <alfredo_petri@hotmail.com>
2024-04-25 14:40:07 -0300

    create paths

---
+++ src/paths.ts
@@ -0,0 +1,9 @@
+export const paths = [
+    { title: "Inicio", link: "https://www.loucaselisas.com.br/" },
+    { title: "Produtos", link: "https://www.loucaselisas.com.br/produtos" },
+    { title: "Sobre", link: "https://www.loucaselisas.com.br/sobre" },
+    { title: "Portfolio", link: "https://www.loucaselisas.com.br/_files/ugd/0914c1_e7d129ccd4cc41829f4cb9256d2cef92.pdf?index=true" },
+    { title: "Academia", link: "https://www.loucaselisas.com.br/academia" },
+    { title: "Contato", link: "https://www.loucaselisas.com.br/contato" },
+    { title: "Comprar", link: "https://www.loucaselisas.com.br/comprar" },
+]
