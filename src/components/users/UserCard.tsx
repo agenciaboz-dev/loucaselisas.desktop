35b98909cb5640cf2329e9905ebfe34768dd87bd
alfredo-petri <alfredo_petri@hotmail.com>
2024-04-26 17:11:44 -0300

    update UserCard

--- src/components/users/UserCard.tsx
+++ src/components/users/UserCard.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Avatar, Box, Grid, IconButton, MenuItem, Paper, Typography } from "@mui/material"
+import { Avatar, Box, Chip, Grid, IconButton, MenuItem, Paper, Typography } from "@mui/material"
 import { User } from "../../types/server/class"
 import { useNavigate } from "react-router-dom"
 import MoreVertIcon from "@mui/icons-material/MoreVert"
@@ -9,28 +9,31 @@ interface UserCardProps {
 }
 
 export const UserCard: React.FC<UserCardProps> = ({ user }) => {
-    const navigate = useNavigate()
+    // const navigate = useNavigate()
 
     return (
         <>
             <Grid item xs={1}>
-                <Paper sx={{ p: "0.5vw", gap: "0.5vw" }}>
-                    <MenuItem>
-                        <Avatar src={user.image || undefined} />
+                <Paper sx={{ padding: "0.7vw" }}>
+                    <MenuItem sx={{ flex: 1, justifyContent: "space-between", p: 0 }} onClick={() => console.log("teste 1")}>
+                        <Box sx={{ alignItems: "center", gap: "0.5vw" }}>
+                            <Avatar src={user.image || undefined} variant="circular" sx={{ width: "4.5vw", height: "4.5vw" }} />
+                            <Box flexDirection="column" gap="0.5vw">
+                                <Box>
+                                    <Typography>{user.name}</Typography>
+                                    <Typography>{user.birth}</Typography>
+                                </Box>
+                                <Box sx={{ gap: "0.3vw" }}>
+                                    {user.admin && <Chip label="ADM" variant="outlined" sx={{ px: "0.5vw" }} />}
+                                    {user.student && <Chip label="Estudante" variant="outlined" sx={{ px: "0.5vw" }} />}
+                                    {user.creator && <Chip label="Criador de Conteudo" variant="outlined" sx={{ px: "0.5vw" }} />}
+                                </Box>
+                            </Box>
+                        </Box>
+                        <IconButton onClick={() => console.log("teste 2")}>
+                            <MoreVertIcon />
+                        </IconButton>
                     </MenuItem>
-                    <Box flexDirection="column">
-                        <Box>
-                            <Typography>{user.name}</Typography>
-                            <Typography>{user.birth}</Typography>
-                        </Box>
-                        <Box>
-                            <Typography>{user.role.name}</Typography>
-                        </Box>
-                    </Box>
-                    <IconButton>
-                        {" "}
-                        <MoreVertIcon />
-                    </IconButton>
                 </Paper>
             </Grid>
         </>
