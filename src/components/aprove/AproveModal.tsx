4f0c78d16eb6ca16dba42fc51d5413433997d304
alfredo-petri <alfredo_petri@hotmail.com>
2024-05-02 11:27:21 -0300

    update AproveModal

--- src/components/aprove/AproveModal.tsx
+++ src/components/aprove/AproveModal.tsx
@@ -1,5 +1,5 @@
-import React, { SetStateAction } from "react"
-import { Box, Modal, Paper, Typography } from "@mui/material"
+import React from "react"
+import { Box, Button, Modal, Paper, Typography } from "@mui/material"
 
 interface AproveModalProps {
     name: string
@@ -11,11 +11,45 @@ interface AproveModalProps {
 export const AproveModal: React.FC<AproveModalProps> = ({ name, type, openAproveModal, handleOpenAproveModal }) => {
     return (
         <Modal open={openAproveModal} onClose={handleOpenAproveModal}>
-            <Paper sx={{ width: "fit-content", h: 1, position: "absolute", top: "30vh", left: "65vw", p: "1vw" }}>
-                {type === "lesson" && <Typography>Deseja realmente APROVAR essa Lição:</Typography>}
-                {type === "course" && <Typography>Deseja realmente APROVAR esse Curso:</Typography>}
-                <Typography>{name}</Typography>
-            </Paper>
+            <Box sx={{ width: "100%", height: "100vh", justifyContent: "center", alignItems: "center" }}>
+                <Paper
+                    sx={{
+                        width: "35vw",
+                        h: 1,
+                        p: "1vw",
+                        flexDirection: "column",
+                        gap: "0.5vw",
+                        borderRadius: "1vw",
+                    }}
+                >
+                    <Box sx={{ flexDirection: "column" }}>
+                        {type === "lesson" && (
+                            <Typography variant="body2" component="h3" sx={{ fontSize: "1.5rem" }}>
+                                Deseja realmente APROVAR essa Lição:
+                            </Typography>
+                        )}
+                        {type === "course" && (
+                            <Typography variant="body2" component="h3" sx={{ fontSize: "1.5rem" }}>
+                                Deseja realmente APROVAR esse Curso:
+                            </Typography>
+                        )}
+                        <Typography variant="subtitle1" component="p" sx={{ fontSize: "1.4rem" }}>
+                            {name}
+                        </Typography>
+                    </Box>
+                    <Typography variant="body1" component="p">
+                        Este conteúdo ficará disponível para todos os usuários cadastrados como estudantes. Deseja mesmo continuar?
+                    </Typography>
+                    <Box sx={{ gap: "0.5vw", mt: "1vw" }}>
+                        <Button fullWidth variant="outlined" sx={{ borderRadius: "3vw" }} onClick={handleOpenAproveModal}>
+                            Cancelar
+                        </Button>
+                        <Button fullWidth variant="contained" sx={{ borderRadius: "3vw" }}>
+                            Aprovar
+                        </Button>
+                    </Box>
+                </Paper>
+            </Box>
         </Modal>
     )
 }
