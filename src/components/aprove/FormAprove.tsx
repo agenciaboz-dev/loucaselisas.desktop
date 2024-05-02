736aa0fb5bec9b41ce666f6eeb9f149bd7fa2545
alfredo-petri <alfredo_petri@hotmail.com>
2024-05-02 12:58:27 -0300

    update FormAprove

--- src/components/aprove/FormAprove.tsx
+++ src/components/aprove/FormAprove.tsx
@@ -1,17 +1,20 @@
 import React, { useState } from "react"
-import { Box, Button, Modal, Paper, Typography } from "@mui/material"
-import { Course } from "../../types/server/class/Course"
-import { Lesson } from "../../types/server/class/Course/Lesson"
+import { Box, Button, Paper } from "@mui/material"
 import { Form } from "../login/Form"
 import { AproveModal } from "./AproveModal"
+import { useFormik } from "formik"
+import { ReproveModal } from "./ReproveModal"
+import { Course, PartialCourse } from "../../types/server/class/Course"
+import { StatusForm } from "../../types/statusForm"
 
 interface FormAproveProps {
     options?: boolean
     name: string
     type: "course" | "lesson"
+    id: string
 }
 
-export const FormAprove: React.FC<FormAproveProps> = ({ options = false, name, type }) => {
+export const FormAprove: React.FC<FormAproveProps> = ({ options = false, name, type, id }) => {
     const [loading, setLoading] = useState(false)
 
     const [openAproveModal, setOpenAproveModal] = useState(false)
@@ -20,22 +23,29 @@ export const FormAprove: React.FC<FormAproveProps> = ({ options = false, name, t
     const handleOpenAproveModal = () => setOpenAproveModal(!openAproveModal)
     const handleopenReproveModal = () => setOpenReproveModal(!openReproveModal)
 
+    const aproveFormik = useFormik<StatusForm>({ initialValues: { id: id, status: "active" }, onSubmit: async (values) => {} })
+    const reproveFormik = useFormik<StatusForm>({
+        initialValues: { id: id, status: "declined", declined_reason: "" },
+        onSubmit: async (values) => {},
+    })
+
     return (
         <Box sx={{}}>
             <Paper sx={{ flex: 1, p: "0.5vw" }}>
-                <Form onSubmit={() => {}} sx={{ flexDirection: "column", flex: 1, gap: "1vw" }}>
+                <Box sx={{ flexDirection: "column", flex: 1, gap: "1vw" }}>
                     {options && <Box></Box>}
 
                     <Box sx={{ justifyContent: "space-between", gap: "0.5vw" }}>
-                        <Button fullWidth variant="outlined" sx={{ borderRadius: "2vw" }}>
+                        <Button fullWidth variant="outlined" sx={{ borderRadius: "2vw" }} onClick={handleopenReproveModal}>
                             Reprovar
                         </Button>
                         <Button fullWidth variant="contained" sx={{ borderRadius: "2vw" }} onClick={handleOpenAproveModal}>
                             Aprovar
                         </Button>
                         <AproveModal name={name} type={type} openAproveModal={openAproveModal} handleOpenAproveModal={handleOpenAproveModal} />
+                        <ReproveModal name={name} type={type} openReproveModal={openReproveModal} handleOpenReproveModal={handleopenReproveModal} />
                     </Box>
-                </Form>
+                </Box>
             </Paper>
         </Box>
     )
