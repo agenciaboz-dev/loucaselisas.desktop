d02127b2c7a8fcebeb0a1cd8b84ef42b271b6e96
alfredo-petri <alfredo_petri@hotmail.com>
2024-05-02 10:10:49 -0300

    update Form Aprove

--- src/components/aprove/FormAprove.tsx
+++ src/components/aprove/FormAprove.tsx
@@ -1,22 +1,40 @@
 import React, { useState } from "react"
-import { Box, Paper } from "@mui/material"
+import { Box, Button, Modal, Paper, Typography } from "@mui/material"
 import { Course } from "../../types/server/class/Course"
 import { Lesson } from "../../types/server/class/Course/Lesson"
 import { Form } from "../login/Form"
+import { AproveModal } from "./AproveModal"
 
 interface FormAproveProps {
-    options: boolean
+    options?: boolean
+    name: string
+    type: "course" | "lesson"
 }
 
-export const FormAprove: React.FC<FormAproveProps> = ({ options = false }) => {
+export const FormAprove: React.FC<FormAproveProps> = ({ options = false, name, type }) => {
     const [loading, setLoading] = useState(false)
+
+    const [openAproveModal, setOpenAproveModal] = useState(false)
+    const [openReproveModal, setOpenReproveModal] = useState(false)
+
+    const handleOpenAproveModal = () => setOpenAproveModal(!openAproveModal)
+    const handleopenReproveModal = () => setOpenReproveModal(!openReproveModal)
 
     return (
         <Box sx={{}}>
-            <Paper>
-                <Form onSubmit={() => {}} sx={{ flexDirection: "column" }}>
+            <Paper sx={{ flex: 1, p: "0.5vw" }}>
+                <Form onSubmit={() => {}} sx={{ flexDirection: "column", flex: 1, gap: "1vw" }}>
                     {options && <Box></Box>}
-                    <Box></Box>
+
+                    <Box sx={{ justifyContent: "space-between", gap: "0.5vw" }}>
+                        <Button fullWidth variant="outlined" sx={{ borderRadius: "2vw" }}>
+                            Reprovar
+                        </Button>
+                        <Button fullWidth variant="contained" sx={{ borderRadius: "2vw" }} onClick={handleOpenAproveModal}>
+                            Aprovar
+                        </Button>
+                        <AproveModal name={name} type={type} openAproveModal={openAproveModal} handleOpenAproveModal={handleOpenAproveModal} />
+                    </Box>
                 </Form>
             </Paper>
         </Box>
