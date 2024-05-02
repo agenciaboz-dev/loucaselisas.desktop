b7e68c8d5bdd47b33c8389b18e13cbd490537c04
alfredo-petri <alfredo_petri@hotmail.com>
2024-05-02 13:02:24 -0300

    update ReproveModal

--- src/components/aprove/ReproveModal.tsx
+++ src/components/aprove/ReproveModal.tsx
@@ -10,5 +10,36 @@ interface ReproveModalProps {
 }
 
 export const ReproveModal: React.FC<ReproveModalProps> = ({ name, type, openReproveModal, handleOpenReproveModal, onSubmit }) => {
-    return <Box></Box>
+    return (
+        <Modal open={openReproveModal} onClose={handleOpenReproveModal}>
+            <Box sx={{ width: "100%", height: "100vh", justifyContent: "center", alignItems: "center" }}>
+                <Paper
+                    sx={{
+                        width: "35vw",
+                        h: 1,
+                        p: "1vw",
+                        flexDirection: "column",
+                        gap: "0.5vw",
+                        borderRadius: "1vw",
+                    }}
+                >
+                    <Box sx={{ flexDirection: "column" }}>
+                        {type === "lesson" && (
+                            <Typography variant="body2" component="h3" sx={{ fontSize: "1.5rem" }}>
+                                Deseja realmente Reprovar essa Lição:
+                            </Typography>
+                        )}
+                        {type === "course" && (
+                            <Typography variant="body2" component="h3" sx={{ fontSize: "1.5rem" }}>
+                                Deseja realmente Reprovar esse Curso:
+                            </Typography>
+                        )}
+                        <Typography variant="subtitle1" component="p" sx={{ fontSize: "1.4rem" }}>
+                            {name}
+                        </Typography>
+                    </Box>
+                </Paper>
+            </Box>
+        </Modal>
+    )
 }
