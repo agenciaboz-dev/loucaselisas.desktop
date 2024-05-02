2e6c6713b34549720ca89e42eaad5ad6b6f9cd9a
alfredo-petri <alfredo_petri@hotmail.com>
2024-05-02 10:10:19 -0300

    update Aprove Modal

--- src/components/aprove/AproveModal.tsx
+++ src/components/aprove/AproveModal.tsx
@@ -1,10 +1,21 @@
-import React from "react"
-import { Box } from "@mui/material"
+import React, { SetStateAction } from "react"
+import { Box, Modal, Paper, Typography } from "@mui/material"
 
 interface AproveModalProps {
     name: string
+    type: "course" | "lesson"
+    openAproveModal: boolean
+    handleOpenAproveModal: () => void
 }
 
-export const AproveModal: React.FC<AproveModalProps> = ({}) => {
-    return <Box sx={{}}></Box>
+export const AproveModal: React.FC<AproveModalProps> = ({ name, type, openAproveModal, handleOpenAproveModal }) => {
+    return (
+        <Modal open={openAproveModal} onClose={handleOpenAproveModal}>
+            <Paper sx={{ width: "fit-content", h: 1, position: "absolute", top: "30vh", left: "65vw", p: "1vw" }}>
+                {type === "lesson" && <Typography>Deseja realmente APROVAR essa Lição:</Typography>}
+                {type === "course" && <Typography>Deseja realmente APROVAR esse Curso:</Typography>}
+                <Typography>{name}</Typography>
+            </Paper>
+        </Modal>
+    )
 }
