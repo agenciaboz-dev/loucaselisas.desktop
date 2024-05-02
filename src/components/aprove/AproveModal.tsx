7cc11917f2f67074e858b5025400568301f9d22c
alfredo-petri <alfredo_petri@hotmail.com>
2024-05-02 09:24:10 -0300

    create AproveModal

---
+++ src/components/aprove/AproveModal.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import { Box } from "@mui/material"
+
+interface AproveModalProps {
+    name: string
+}
+
+export const AproveModal: React.FC<AproveModalProps> = ({}) => {
+    return <Box sx={{}}></Box>
+}
