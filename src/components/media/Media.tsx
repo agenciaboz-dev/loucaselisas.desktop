fabdf518fc11c5eb4e28d61f4e57cd63347a269c
alfredo-petri <alfredo_petri@hotmail.com>
2024-05-06 11:08:21 -0300

    update Media

--- src/components/media/Media.tsx
+++ src/components/media/Media.tsx
@@ -1,8 +1,37 @@
-import React from "react"
-import { Box } from "@mui/material"
+import React, { SetStateAction } from "react"
+import { Avatar, Paper } from "@mui/material"
 
-interface MediaProps {}
+interface MediaProps {
+    setShowCarrosel: React.Dispatch<SetStateAction<boolean>>
+    media: {
+        url: string
+        type: "image" | "video"
+    }
+}
 
-export const Media: React.FC<MediaProps> = ({}) => {
-    return <Box sx={{}}></Box>
+export const Media: React.FC<MediaProps> = ({ setShowCarrosel, media }) => {
+    return (
+        <>
+            <Paper
+                sx={{ borderRadius: "1vw", width: "42.5vw", aspectRatio: "16/9" }}
+                onMouseEnter={() => setShowCarrosel(true)}
+                onMouseLeave={() => setShowCarrosel(false)}
+            >
+                {media.type === "video" && (
+                    <video
+                        src={media.url}
+                        controls
+                        style={{
+                            borderRadius: "1vw",
+                            width: "100%",
+                            height: "100%",
+                        }}
+                    />
+                )}
+                {media.type === "image" && (
+                    <Avatar variant="rounded" src={media.url} sx={{ width: 1, height: 1, objectFit: "contain", borderRadius: "1vw" }} />
+                )}
+            </Paper>
+        </>
+    )
 }
