7c90f39e676b3d8118d0e0e462924679a82c1de8
alfredo-petri <alfredo_petri@hotmail.com>
2024-05-06 10:48:05 -0300

    update carrousel

--- src/components/Carrousel.tsx
+++ src/components/Carrousel.tsx
@@ -1,6 +1,7 @@
-import React from "react"
-import { Avatar, Box } from "@mui/material"
+import React, { useRef } from "react"
+import { Avatar, Box, MenuItem } from "@mui/material"
 import PlayArrowIcon from "@mui/icons-material/PlayArrow"
+import { useDraggable } from "react-use-draggable-scroll"
 
 interface CarrouselProps {
     gallery: { url: string; type: "image" | "video" }[]
@@ -10,11 +11,16 @@ interface CarrouselProps {
     isVideo: boolean
 }
 
-const mediaDesign = { width: "8vw", height: "5vw", borderRadius: "1vw", cursor: "pointer" }
+const mediaDesign = { width: "8vw", height: "5vw", borderRadius: "1vw", padding: 0 }
 
 export const Carrousel: React.FC<CarrouselProps> = ({ gallery, setMedia, onMouseEnter, onMouseLeave, isVideo }) => {
+    const scrollRef = useRef<HTMLElement>() as React.MutableRefObject<HTMLInputElement>
+    const { events } = useDraggable(scrollRef, { applyRubberBandEffect: true })
+
     return (
         <Box
+            ref={scrollRef}
+            {...events}
             sx={{
                 gap: "1vw",
                 position: "absolute",
@@ -26,25 +32,22 @@ export const Carrousel: React.FC<CarrouselProps> = ({ gallery, setMedia, onMouse
                 alignItems: "center",
                 pl: "1vw",
                 borderRadius: isVideo ? 0 : "0 0 1vw 1vw",
+                overflowX: "auto",
+                scrollbarWidth: "none",
             }}
             onMouseEnter={onMouseEnter}
             onMouseLeave={onMouseLeave}
         >
             {gallery.map((media) => (
-                <>
-                    {media.type === "image" && (
-                        <Avatar key={media.url} src={media.url} sx={mediaDesign} onClick={() => setMedia({ type: media.type, url: media.url })} />
-                    )}
+                <MenuItem onClick={() => setMedia({ type: media.type, url: media.url })} sx={{ ...mediaDesign, userSelect: "none" }}>
+                    {media.type === "image" && <Avatar key={media.url} src={media.url} sx={{ ...mediaDesign, pointerEvents: "none" }} />}
                     {media.type === "video" && (
-                        <Box
-                            sx={{ ...mediaDesign, position: "relative", alignItems: "center", justifyContent: "center" }}
-                            onClick={() => setMedia({ type: media.type, url: media.url })}
-                        >
-                            <video key={media.url} src={media.url} style={mediaDesign}></video>
+                        <Box sx={{ ...mediaDesign, position: "relative", alignItems: "center", justifyContent: "center" }}>
+                            <video key={media.url} src={media.url} style={{ ...mediaDesign, pointerEvents: "none" }}></video>
                             <PlayArrowIcon color="secondary" sx={{ position: "absolute", fontSize: "2.5rem" }} />
                         </Box>
                     )}
-                </>
+                </MenuItem>
             ))}
         </Box>
     )
