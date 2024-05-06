d7cf87c7b03d71f6285c0b64a21d99f064d34c9b
alfredo-petri <alfredo_petri@hotmail.com>
2024-05-06 14:05:32 -0300

    update Lesson

--- src/pages/lessons/Lesson.tsx
+++ src/pages/lessons/Lesson.tsx
@@ -1,8 +1,17 @@
 import React from "react"
 import { Box } from "@mui/material"
+import { HeaderInfo } from "../../components/header/HeaderInfo"
+import { useLocation } from "react-router-dom"
+import { Lesson } from "../../types/server/class/Course/Lesson"
 
-interface LessonProps {}
+interface LessonPageProps {}
 
-export const Lesson: React.FC<LessonProps> = ({}) => {
-    return <Box sx={{ flexDirection: "column" }}></Box>
+export const LessonPage: React.FC<LessonPageProps> = ({}) => {
+    const lesson = useLocation().state.data as Lesson
+
+    return (
+        <Box sx={{ flexDirection: "column" }}>
+            <HeaderInfo title={`Lição: ${lesson.name}`} backButton exitButton={false} refreshButton={false} chatButton={true} />
+        </Box>
+    )
 }
