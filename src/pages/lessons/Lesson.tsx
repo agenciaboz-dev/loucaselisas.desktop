17093ba355c5f5b040018dcb66fb7e9c443bb045
alfredo-petri <alfredo_petri@hotmail.com>
2024-05-06 11:01:58 -0300

    create Lesson

---
+++ src/pages/lessons/Lesson.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+import { Box } from "@mui/material"
+
+interface LessonProps {}
+
+export const Lesson: React.FC<LessonProps> = ({}) => {
+    return <Box sx={{ flexDirection: "column" }}></Box>
+}
