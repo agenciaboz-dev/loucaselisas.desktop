5d9e985c5acdef57b0b50a92a271c0a0577cc1d7
alfredo-petri <alfredo_petri@hotmail.com>
2024-05-06 16:10:58 -0300

    Update Lesson

--- src/pages/lessons/Lesson.tsx
+++ src/pages/lessons/Lesson.tsx
@@ -1,17 +1,62 @@
-import React from "react"
-import { Box } from "@mui/material"
+import React, { useEffect, useState } from "react"
+import { Box, Grid } from "@mui/material"
 import { HeaderInfo } from "../../components/header/HeaderInfo"
 import { useLocation } from "react-router-dom"
 import { Lesson } from "../../types/server/class/Course/Lesson"
+import { Media } from "../../components/media/Media"
+import { Carrousel } from "../../components/Carrousel"
+import { api } from "../../api/api"
 
 interface LessonPageProps {}
 
 export const LessonPage: React.FC<LessonPageProps> = ({}) => {
     const lesson = useLocation().state.data as Lesson
 
+    const [loading, setLoading] = useState(false)
+    const [lessons, setLessons] = useState<Lesson[]>([])
+
+    const fetchLessons = async () => {
+        if (loading) return
+        setLoading(true)
+        try {
+            const response = await api.get("/lesson/course", { params: { course_id: lesson.course_id } })
+            setLessons(response.data)
+        } catch (error) {
+            console.log(error)
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        fetchLessons()
+    }, [])
+    const medias = [{ url: lesson.thumb, type: lesson.media.type }, ...lessons.map((lesson) => ({ url: lesson.thumb, type: lesson.media.type }))]
+
+    const [media, setMedia] = useState({ url: lesson.thumb, type: lesson.media.type })
+    const [showCarrosel, setShowCarrosel] = useState(false)
+
     return (
         <Box sx={{ flexDirection: "column" }}>
-            <HeaderInfo title={`Lição: ${lesson.name}`} backButton exitButton={false} refreshButton={false} chatButton menuButton />
+            <Grid container spacing={3} sx={{ width: "75vw", height: "74vh" }}>
+                <Grid item xs={7}>
+                    <Box sx={{ w: 1, h: 1, flexDirection: "column", gap: "1vw" }}>
+                        <HeaderInfo title={`Lição: ${lesson.name}`} backButton exitButton={false} refreshButton={false} chatButton menuButton />
+                        <Box sx={{ w: 1, h: 1, position: "relative", flexDirection: "column" }}>
+                            <Media media={media} setShowCarrosel={setShowCarrosel} />
+                            {showCarrosel && (
+                                <Carrousel
+                                    setMedia={setMedia}
+                                    isVideo={media.type === "video"}
+                                    gallery={medias}
+                                    onMouseEnter={() => setShowCarrosel(true)}
+                                    onMouseLeave={() => setShowCarrosel(false)}
+                                />
+                            )}
+                        </Box>
+                    </Box>
+                </Grid>
+            </Grid>
         </Box>
     )
 }
