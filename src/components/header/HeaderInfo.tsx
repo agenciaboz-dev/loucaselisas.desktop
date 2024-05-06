7e41ad8ed7173ac0f0b5cd51fc017b162577b5fc
alfredo-petri <alfredo_petri@hotmail.com>
2024-05-06 14:55:29 -0300

    update HeaderInfo

--- src/components/header/HeaderInfo.tsx
+++ src/components/header/HeaderInfo.tsx
@@ -6,6 +6,8 @@ import LogoutIcon from "@mui/icons-material/Logout"
 import { useUser } from "../../hooks/useUser"
 import { IoMdAddCircleOutline } from "react-icons/io"
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import ChatIcon from "@mui/icons-material/Chat"
+import MoreVertIcon from "@mui/icons-material/MoreVert"
 interface HeaderInfoProps {
     title: string
     loading?: boolean
@@ -14,6 +16,8 @@ interface HeaderInfoProps {
     dashButton?: boolean
     exitButton?: boolean
     backButton?: boolean
+    chatButton?: boolean
+    menuButton?: boolean
 }
 
 export const HeaderInfo: React.FC<HeaderInfoProps> = ({
@@ -23,33 +27,59 @@ export const HeaderInfo: React.FC<HeaderInfoProps> = ({
     refreshButton = true,
     dashButton = false,
     exitButton = true,
-    backButton = false
+    backButton = false,
+    chatButton = false,
+    menuButton = false,
 }) => {
     const { onLogout } = useUser()
     const navigate = useNavigate()
     return (
-        <Box sx={{ alignItems: "center", justifyContent: "space-between" }}>
-            <Box sx={{ alignItems: "center", gap: "0.7vw" }}>
-                {backButton && <IconButton onClick={()=> navigate("/cursos")}><ArrowBackIosIcon /></IconButton>}
-                <Typography variant="h1" component="h1" sx={{ textTransform: "uppercase" }}>
-                    {title}
-                </Typography>
-                {refreshButton && <RefreshButton loading={loading} callBack={refreshCallback} />}
-                {dashButton && (
-                    <Button variant="outlined" sx={{ border: "1px dashed", width: "fit-content", gap: "0.3vw", borderRadius: "1vw" }}>
-                        Adicionar novo usuário
-                        <IoMdAddCircleOutline size={"1.3vw"} />
+        <Box sx={{ alignItems: "center", justifyContent: "space-between", border: "1px solid red" }}>
+            <Box sx={{ alignItems: "center", gap: "0.2vw", width: 1 }}>
+                {backButton && (
+                    <IconButton onClick={() => navigate("/cursos")}>
+                        <ArrowBackIosIcon />
+                    </IconButton>
+                )}
+                <Box sx={{ alignItems: "center", flex: 1, gap: "0.5vw" }}>
+                    <Typography
+                        variant="h1"
+                        component="h1"
+                        sx={{ textTransform: "uppercase", maxWidth: "25vw", overflow: "hidden", textOverflow: "ellipsis", whiteSpace: "nowrap" }}
+                    >
+                        {title}
+                    </Typography>
+
+                    {refreshButton && <RefreshButton loading={loading} callBack={refreshCallback} />}
+
+                    {dashButton && (
+                        <Button variant="outlined" sx={{ border: "1px dashed", width: "fit-content", gap: "0.3vw", borderRadius: "1vw" }}>
+                            Adicionar novo usuário
+                            <IoMdAddCircleOutline size={"1.3vw"} />
+                        </Button>
+                    )}
+
+                    {!!chatButton && (
+                        <Button variant="contained" endIcon={<ChatIcon />} sx={{ p: "0.2vw 1vw", borderRadius: "2vw", ml: "auto" }}>
+                            Ir para grupo
+                        </Button>
+                    )}
+
+                    {!!menuButton && (
+                        <IconButton>
+                            <MoreVertIcon />
+                        </IconButton>
+                    )}
+                </Box>
+
+                {exitButton && (
+                    <Button endIcon={<LogoutIcon />} onClick={onLogout}>
+                        <Typography variant="h4" component="p" sx={{ fontSize: "1.4rem" }}>
+                            SAIR
+                        </Typography>
                     </Button>
                 )}
             </Box>
-
-            {exitButton && (
-                <Button endIcon={<LogoutIcon />} onClick={onLogout}>
-                    <Typography variant="h4" component="p" sx={{ fontSize: "1.4rem" }}>
-                        SAIR
-                    </Typography>
-                </Button>
-            )}
         </Box>
     )
 }
