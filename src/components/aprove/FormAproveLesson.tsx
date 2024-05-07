e7a567d14556e92a018002c16f76341b6f0bc983
alfredo-petri <alfredo_petri@hotmail.com>
2024-05-07 13:49:44 -0300

    create FormAproveLesson

---
+++ src/components/aprove/FormAproveLesson.tsx
@@ -0,0 +1,97 @@
+import React, { useState } from "react"
+import { Box, Button, Paper } from "@mui/material"
+import { useFormik } from "formik"
+import { StatusForm } from "../../types/statusForm"
+import { api } from "../../api/api"
+import { AproveModal } from "./AproveModal"
+import { ReproveModal } from "./ReproveModal"
+
+interface FormAproveLessonProps {
+    id: string
+    name: string
+    type: "course" | "lesson"
+}
+
+export const FormAproveLesson: React.FC<FormAproveLessonProps> = ({ id, name, type }) => {
+    const [loading, setLoading] = useState(false)
+
+    const [openAproveModal, setopenAproveModal] = useState(false)
+    const [openReproveModal, setOpenReproveModal] = useState(false)
+
+    const handleOpenAproveModal = () => setopenAproveModal(!openAproveModal)
+    const handleopenReproveModal = () => setOpenReproveModal(!openReproveModal)
+
+    const formik = useFormik<StatusForm>({
+        initialValues: {
+            id: id,
+            status: "active",
+        },
+        onSubmit: async (values) => {
+            if (loading) return
+            setLoading(true)
+
+            try {
+                const response = await api.patch("/lesson", values)
+                setopenAproveModal(!setopenAproveModal)
+                console.log(response.data)
+            } catch (error) {
+                console.log(error)
+            } finally {
+                setTimeout(() => {
+                    setLoading(false)
+                }, 500)
+            }
+        },
+    })
+
+    const onReprove = async (reason: string) => {
+        const data: StatusForm = { id: id, status: "declined", declined_reason: reason }
+        console.log(data)
+        if (loading) return
+        setLoading(true)
+
+        try {
+            const response = await api.patch("/lesson", data)
+            setOpenReproveModal(!openReproveModal)
+
+            console.log(response.data)
+        } catch (error) {
+            console.log(error)
+        } finally {
+            setTimeout(() => {
+                setLoading(false)
+            }, 500)
+        }
+    }
+
+    return (
+        <Box sx={{}}>
+            <Paper sx={{ flex: 1, p: "0.7vw", borderRadius: "1vw" }}>
+                <Box sx={{ flexDirection: "column", flex: 1, gap: "1vw" }}>
+                    <Box sx={{ justifyContent: "space-between", gap: "0.5vw" }}>
+                        <Button fullWidth variant="outlined" sx={{ borderRadius: "2vw" }} onClick={handleopenReproveModal}>
+                            Reprovar
+                        </Button>
+                        <Button fullWidth variant="contained" sx={{ borderRadius: "2vw" }} onClick={handleOpenAproveModal}>
+                            Aprovar
+                        </Button>
+                        <AproveModal
+                            name={name}
+                            type={type}
+                            openAproveModal={openAproveModal}
+                            handleOpenAproveModal={handleOpenAproveModal}
+                            onConfirm={formik.handleSubmit}
+                        />
+                        <ReproveModal
+                            name={name}
+                            type={type}
+                            openReproveModal={openReproveModal}
+                            handleOpenReproveModal={handleopenReproveModal}
+                            onConfirm={onReprove}
+                        />
+                    </Box>
+                </Box>
+            </Paper>
+        </Box>
+    )
+}
