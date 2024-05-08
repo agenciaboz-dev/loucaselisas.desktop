3a93c4b39e11b2f603035e98a53ddda20c10e545
alfredo-petri <alfredo_petri@hotmail.com>
2024-05-08 12:34:17 -0300

    update statusForm

--- src/types/statusForm.d.ts
+++ src/types/statusForm.d.ts
@@ -1,8 +1,10 @@
 import { Status } from "./server/class/Course"
+import { Plan } from "./server/class/Plan"
 
 export interface StatusForm {
     id: string
     status: Status
     declined_reason?: string
     price?: number
+    plan?: Plan
 }
