0bcec6395247eb61b9de63c9639311ebffe74045
alfredo-petri <alfredo_petri@hotmail.com>
2024-05-09 16:17:43 -0300

    update Settings

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,39 +1,82 @@
-import React from "react"
-import { Box, Checkbox, Divider, Grid, IconButton, Paper, TextField, Typography } from "@mui/material"
+import React, { useEffect, useState } from "react"
+import { Box, Button, Dialog, DialogContent, DialogTitle, Grid, Paper, TextField, Typography } from "@mui/material"
 import { HeaderInfo } from "../components/header/HeaderInfo"
 import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline"
 import { useFormik } from "formik"
-import { CategoryForm } from "../types/server/class/Category"
+import { Category, CategoryForm } from "../types/server/class/Category"
+import { Avatar, FileInputButton } from "@files-ui/react"
+import { api } from "../api/api"
+import { Form } from "../components/login/Form"
 
 interface SettingsProps {}
 
 export const Settings: React.FC<SettingsProps> = ({}) => {
-    const label = { inputProps: { "aria-label": "categorias" } }
+    const [imageSource, setImageSource] = useState<File>()
 
-    const formikCategories = useFormik<CategoryForm>({ initialValues: { name: "" }, onSubmit: () => {} })
-    const formikPlans = useFormik({ initialValues: {}, onSubmit: () => {} })
+    const [openModal, setOpenModal] = useState(false)
 
     return (
         <Box sx={{ flexDirection: "column", flex: 1 }}>
             <HeaderInfo title="Configurações" exitButton={false} refreshButton={false} />
             <Grid container columns={3} spacing={2} sx={{ pt: "1vw" }}>
                 <Grid item xs={1}>
-                    <Box sx={{ flexDirection: "column", gap: "0.5vw" }}>
+                    <Box sx={{ flexDirection: "column", gap: "1vw" }}>
                         <Typography>Categorias</Typography>
-                        <Box sx={{ justifyContent: "space-between" }}>
-                            <Box sx={{ alignItems: "center" }}>
-                                <Checkbox aria-label="selecionar todos" />
-                                <Typography>Selecionar todos</Typography>
-                            </Box>
-                            <IconButton>
-                                <AddCircleOutlineIcon />
-                            </IconButton>
+                        <Box sx={{ flexDirection: "column", gap: "0.5vw" }}>
+                            <Button
+                                startIcon={<AddCircleOutlineIcon />}
+                                sx={{ borderRadius: "5vw", border: "1px dashed" }}
+                                onClick={() => {
+                                    setOpenModal(!openModal)
+                                }}
+                            >
+                                Adicionar Categoria
+                            </Button>
                         </Box>
-                        <Divider sx={{ mt: "-0.5vw" }} />
-                        <Paper sx={{ p: "12px 12px 12px 0.3vw", gap: "0.3vw", borderRadius: "0.8vw" }}>
-                            <Checkbox />
-                            <TextField label="Nome" placeholder="Nome do plano" sx={{ flex: 1 }} />
-                        </Paper>
+                        <Dialog
+                            open={openModal}
+                            onClose={() => setOpenModal(!openModal)}
+                            PaperProps={{ sx: { width: "100%", maxWidth: "fit-content", padding: "1vw", gap: "1vw", borderRadius: "1.2vw" } }}
+                        >
+                            <Box>
+                                <Typography variant="body1" component="p" sx={{ fontSize: "1.1rem" }}>
+                                    Adicionar Categoria
+                                </Typography>
+                            </Box>
+                            <Box sx={{ gap: "1vw", width: "100%" }}>
+                                <Avatar readOnly src={imageSource || "/placeholders/perfil.webp"} style={{ width: "135px", height: "135px" }} />
+                                <Box sx={{ flexDirection: "column", gap: "1vw" }}>
+                                    <Box sx={{ flexDirection: "column", gap: "0.2vw" }}>
+                                        <Typography>Nome da Categoria</Typography>
+                                        <TextField name="name" sx={{ width: "20vw" }} />
+                                    </Box>
+                                    <Box sx={{ gap: "1vw", alignItems: "center", justifyContent: "space-between" }}>
+                                        <Typography>Adicionar a imagem a categoria</Typography>
+                                        <FileInputButton
+                                            label="Procurar"
+                                            accept="image/*"
+                                            onChange={(files) => setImageSource(files[0].file)}
+                                            style={{ textTransform: "none", borderRadius: "5vw", backgroundColor: "#88827C" }}
+                                        />
+                                    </Box>
+                                </Box>
+                            </Box>
+                            <Box sx={{ gap: "1vw" }}>
+                                <Button
+                                    variant="outlined"
+                                    sx={{ flex: 1, borderRadius: "5vw" }}
+                                    onClick={() => {
+                                        setOpenModal(!openModal)
+                                    }}
+                                >
+                                    Cancelar
+                                </Button>
+                                <Button type="submit" variant="contained" sx={{ flex: 1, borderRadius: "5vw" }}>
+                                    Adicionar
+                                </Button>
+                            </Box>
+                        </Dialog>
+                        {<Paper></Paper>}
                     </Box>
                 </Grid>
                 <Grid item xs={1}>
