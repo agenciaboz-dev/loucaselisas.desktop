cfbfbe240d175cc334e63c6368e82f2396b9f973
alfredo-petri <alfredo_petri@hotmail.com>
2024-05-10 10:12:36 -0300

    create SettingsCard

---
+++ src/components/settings/SettingsCard.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react"
+import { Avatar, Box, IconButton, Paper, Typography } from "@mui/material"
+import DeleteIcon from "@mui/icons-material/Delete"
+import EditIcon from "@mui/icons-material/Edit"
+
+interface SettingCardProps {
+    image: string
+    name: string
+    openEditModal: React.Dispatch<React.SetStateAction<boolean>>
+    removeCategory?: () => void
+}
+
+export const SettingsCard: React.FC<SettingCardProps> = ({ image, name, openEditModal, removeCategory }) => {
+    const [onHover, setOnHover] = useState(false)
+
+    return (
+        <Paper onMouseEnter={() => setOnHover(true)} onMouseLeave={() => setOnHover(false)} sx={{ w: 1, height: "3.5vw", p: "0.5vw" }}>
+            <Box sx={{ w: 1, alignItems: "center", justifyContent: "space-between" }}>
+                <Box sx={{ gap: "0.5vw", flex: 1, alignItems: "center", width: "17.8vw" }}>
+                    <Avatar src={image} sx={{ width: "2.5vw", height: "2.5vw" }} />
+                    <Typography variant="body2" component="p" sx={{ fontSize: "1.1rem" }}>
+                        {name}
+                    </Typography>
+                </Box>
+                <Box sx={{ width: "5vw", height: 1, gap: "0.2vw", marginLeft: "auto" }}>
+                    {onHover && (
+                        <>
+                            <IconButton onClick={removeCategory}>
+                                <DeleteIcon color="error" />
+                            </IconButton>
+                            <IconButton onClick={() => openEditModal(true)}>
+                                <EditIcon color="primary" />
+                            </IconButton>
+                        </>
+                    )}
+                </Box>
+            </Box>
+        </Paper>
+    )
+}
