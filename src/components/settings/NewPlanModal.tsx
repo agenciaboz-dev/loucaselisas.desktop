d454aaebdb6e01d39b62f106eafb968a9f6bb81b
alfredo-petri <alfredo_petri@hotmail.com>
2024-05-10 14:15:47 -0300

    update NewPlanModal

--- src/components/settings/NewPlanModal.tsx
+++ src/components/settings/NewPlanModal.tsx
@@ -1,40 +1,55 @@
 import React from "react"
-import { Box, Button, Dialog, TextField, Typography } from "@mui/material"
-import { BooleanSchema } from "yup"
+import { Box, Button, Dialog, MenuItem, TextField, Typography } from "@mui/material"
 
 interface NewPlanModalProps {
-    formik: {
+    formik?: {
         handleChange: (e: React.ChangeEvent<any>) => void
         handleSubmit: (e?: React.FormEvent<HTMLFormElement> | undefined) => void
     }
 
-    openModal: boolean
-    setOpenModal: React.Dispatch<React.SetStateAction<boolean>>
+    openPlanModal: boolean
+    setOpenPlanModal: React.Dispatch<React.SetStateAction<boolean>>
 }
 
-export const NewPlanModal: React.FC<NewPlanModalProps> = ({ formik, openModal, setOpenModal }) => {
+export const NewPlanModal: React.FC<NewPlanModalProps> = ({ formik, openPlanModal, setOpenPlanModal }) => {
     return (
         <Dialog
-            open={openModal}
-            onClose={() => setOpenModal(!openModal)}
+            open={openPlanModal}
+            onClose={() => setOpenPlanModal(false)}
             PaperProps={{ sx: { width: "100%", maxWidth: "fit-content", padding: "1vw", gap: "1vw", borderRadius: "1.2vw" } }}
         >
-            <form onSubmit={formik.handleSubmit}>
+            <form onSubmit={formik?.handleSubmit}>
                 <Box>
-                    <Typography variant="body1" component="p" sx={{ fontSize: "1.1rem" }}>
-                        Adicionar Categoria
+                    <Typography variant="body1" component="p" sx={{ fontSize: "1.5rem" }}>
+                        Adicionar Plano
                     </Typography>
                 </Box>
                 <Box sx={{ gap: "1vw", width: "100%" }}>
                     <Box sx={{ flexDirection: "column", gap: "1vw" }}>
                         <Box sx={{ flexDirection: "column", gap: "0.2vw" }}>
-                            <Typography>Nome da Categoria</Typography>
-                            //! implementar o value no TextField
-                            {/* value={formik.values.name} */}
-                            <TextField name="name" onChange={formik.handleChange} sx={{ width: "20vw" }} />
+                            <Typography>Nome do Plano</Typography>
+                            <TextField name="name" onChange={formik?.handleChange} sx={{ width: "30vw" }} />
                         </Box>
-                        <Box sx={{ gap: "1vw", alignItems: "center", justifyContent: "space-between" }}>
-                            <Typography>Adicionar a imagem a categoria</Typography>
+                        <Box sx={{ flexDirection: "column", gap: "0.2vw" }}>
+                            <Typography>Descrição do Plano</Typography>
+                            <TextField name="desciprition" onChange={formik?.handleChange} sx={{ width: "30vw" }} />
+                        </Box>
+                        <Box sx={{ gap: "1vw", alignItems: "center" }}>
+                            <Box sx={{ flexDirection: "column", gap: "0.2vw" }}>
+                                <Typography>Valor</Typography>
+                                <TextField name="price" onChange={formik?.handleChange} sx={{ width: "13.5vw" }} />
+                            </Box>
+                            <Typography variant="body1" component="p" sx={{ mt: "15px" }}>
+                                por
+                            </Typography>
+                            <Box sx={{ flexDirection: "column", gap: "0.2vw" }}>
+                                <Typography>Periodo</Typography>
+                                <TextField select name="duration" onChange={formik?.handleChange} sx={{ width: "13.5vw" }}>
+                                    <MenuItem>teste1</MenuItem>
+                                    <MenuItem>teste2</MenuItem>
+                                    <MenuItem>teste3</MenuItem>
+                                </TextField>
+                            </Box>
                         </Box>
                     </Box>
                 </Box>
@@ -43,7 +58,7 @@ export const NewPlanModal: React.FC<NewPlanModalProps> = ({ formik, openModal, s
                         variant="outlined"
                         sx={{ flex: 1, borderRadius: "5vw" }}
                         onClick={() => {
-                            setOpenModal(!openModal)
+                            setOpenPlanModal(false)
                         }}
                     >
                         Cancelar
