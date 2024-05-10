79a72b4286a00f5d6da393f4e37827021f01eae0
alfredo-petri <alfredo_petri@hotmail.com>
2024-05-10 12:43:47 -0300

    feat list categories && plans

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -7,14 +7,18 @@ import { Category, CategoryForm } from "../types/server/class/Category"
 import { Avatar, FileInputButton } from "@files-ui/react"
 import { api } from "../api/api"
 import { NewCategoryModal } from "../components/settings/NewCategoryModal"
+import { SettingsCard } from "../components/settings/SettingsCard"
+import { Plan } from "../types/server/class/Plan"
 
 interface SettingsProps {}
 
 export const Settings: React.FC<SettingsProps> = ({}) => {
     const [loading, setLoading] = useState(false)
     const [categories, setCategorys] = useState<Category[]>([])
+    const [plans, setPlans] = useState<Plan[]>([])
     const [imageSource, setImageSource] = useState<File>()
-    const [openModal, setOpenModal] = useState(false)
+    const [openCategoryModal, setOpenCategoryModal] = useState(false)
+    const [openPlanModal, setOpenPlanModal] = useState(false)
 
     const fetchCategories = async () => {
         if (loading) return
@@ -37,6 +41,35 @@ export const Settings: React.FC<SettingsProps> = ({}) => {
         fetchCategories()
     }, [categories])
 
+    // const deleteCategories = async () => {
+    //     if (loading) return
+    //     setLoading(true)
+
+    //     try {
+    //         const response = await api.delete("/")
+    //     } catch (error) {}
+    // }
+
+    const fetchPlans = async () => {
+        if (loading) return
+        setLoading(true)
+
+        try {
+            const response = await api.get("/plan")
+            setPlans(response.data)
+        } catch (error) {
+            console.log(error)
+        } finally {
+            setTimeout(() => {
+                setLoading(false)
+            }, 500)
+        }
+    }
+
+    useEffect(() => {
+        fetchPlans()
+    }, [])
+
     const formikCategories = useFormik<CategoryForm>({
         initialValues: { name: "" },
         onSubmit: async () => {
@@ -46,7 +79,7 @@ export const Settings: React.FC<SettingsProps> = ({}) => {
             try {
                 const response = await api.post("/category", formikCategories.values)
                 console.log(response.data)
-                setOpenModal(!openModal)
+                setOpenCategoryModal(!openCategoryModal)
                 fetchCategories()
             } catch (error) {
                 console.log(error)
@@ -70,27 +103,65 @@ export const Settings: React.FC<SettingsProps> = ({}) => {
                                 startIcon={<AddCircleOutlineIcon />}
                                 sx={{ borderRadius: "5vw", border: "1px dashed" }}
                                 onClick={() => {
-                                    setOpenModal(!openModal)
+                                    setOpenCategoryModal(!openCategoryModal)
                                 }}
                             >
                                 Adicionar Categoria
                             </Button>
                         </Box>
-
+                        <Box
+                            sx={{
+                                flexDirection: "column",
+                                gap: "0.5vw",
+                                p: " 0.1vw 0.5vw 1vw 0.2vw ",
+                                overflow: "scroll",
+                                maxHeight: "61.8vh",
+                            }}
+                        >
+                            {categories.map((category) => (
+                                <SettingsCard key={category.id} image={category.cover} name={category.name} openEditModal={setOpenCategoryModal} />
+                            ))}
+                        </Box>
                         <NewCategoryModal
                             formik={formikCategories}
                             imageSource={imageSource}
                             setImageSource={setImageSource}
-                            openModal={openModal}
-                            setOpenModal={setOpenModal}
+                            openModal={openCategoryModal}
+                            setOpenModal={setOpenCategoryModal}
                         />
                     </Box>
                 </Grid>
                 <Grid item xs={1}>
-                    <Typography>Planos</Typography>
+                    <Box sx={{ flexDirection: "column", gap: "1vw" }}>
+                        <Typography>Planos</Typography>
+                        <Box sx={{ flexDirection: "column", gap: "0.5vw" }}>
+                            <Button
+                                startIcon={<AddCircleOutlineIcon />}
+                                sx={{ borderRadius: "5vw", border: "1px dashed" }}
+                                onClick={() => {
+                                    setOpenPlanModal(!openPlanModal)
+                                }}
+                            >
+                                Adicionar Planos
+                            </Button>
+                        </Box>
+                        <Box
+                            sx={{
+                                flexDirection: "column",
+                                gap: "0.5vw",
+                                p: " 0.1vw 0.5vw 1vw 0.2vw ",
+                                overflow: "scroll",
+                                maxHeight: "61.8vh",
+                            }}
+                        >
+                            {plans.map((plan) => (
+                                <SettingsCard key={plan.id} name={plan.name} openEditModal={setOpenPlanModal} plan />
+                            ))}
+                        </Box>
+                    </Box>
                 </Grid>
                 <Grid item xs={1}>
-                    <Typography>Estatisticas & Relatórios</Typography>
+                    <Box sx={{ flexDirection: "column", gap: "1vw" }}>Graficos</Box>
                 </Grid>
             </Grid>
         </Box>
