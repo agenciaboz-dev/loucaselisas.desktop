10194eff888e6461062cea62ca41151ebac74add
alfredo-petri <alfredo_petri@hotmail.com>
2024-05-10 12:42:20 -0300

    fix design errors

--- src/components/settings/SettingsCard.tsx
+++ src/components/settings/SettingsCard.tsx
@@ -4,20 +4,21 @@ import DeleteIcon from "@mui/icons-material/Delete"
 import EditIcon from "@mui/icons-material/Edit"
 
 interface SettingCardProps {
-    image: string
+    image?: string
     name: string
+    plan?: boolean
     openEditModal: React.Dispatch<React.SetStateAction<boolean>>
     removeCategory?: () => void
 }
 
-export const SettingsCard: React.FC<SettingCardProps> = ({ image, name, openEditModal, removeCategory }) => {
+export const SettingsCard: React.FC<SettingCardProps> = ({ image, name, openEditModal, removeCategory, plan = false }) => {
     const [onHover, setOnHover] = useState(false)
 
     return (
         <Paper onMouseEnter={() => setOnHover(true)} onMouseLeave={() => setOnHover(false)} sx={{ w: 1, height: "3.5vw", p: "0.5vw" }}>
             <Box sx={{ w: 1, alignItems: "center", justifyContent: "space-between" }}>
                 <Box sx={{ gap: "0.5vw", flex: 1, alignItems: "center", width: "17.8vw" }}>
-                    <Avatar src={image} sx={{ width: "2.5vw", height: "2.5vw" }} />
+                    {!plan && <Avatar src={image} sx={{ width: "2.5vw", height: "2.5vw" }} />}
                     <Typography variant="body2" component="p" sx={{ fontSize: "1.1rem" }}>
                         {name}
                     </Typography>
