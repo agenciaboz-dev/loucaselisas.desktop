e3edf8a82e958b399a36cc70ea4fa12c97cdabf2
alfredo-petri <alfredo_petri@hotmail.com>
2024-05-13 08:48:41 -0300

    fix design error

--- src/components/dashboard/CoursesList.tsx
+++ src/components/dashboard/CoursesList.tsx
@@ -43,7 +43,7 @@ export const CoursesList: React.FC<CoursesListProps> = ({ course }) => {
                         {course.description}
                     </Typography>
                     <Typography variant="body2" component="p" sx={{ fontSize: "0.8rem" }}>
-                        {course.lessons || 5999} Videos
+                        {course.lessons || 5999} Aulas
                     </Typography>
                 </Box>
             </Box>
