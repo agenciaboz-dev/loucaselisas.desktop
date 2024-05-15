bbc265a267fdc625569a1af6daed6c1e92b4e43e
alfredo-petri <alfredo_petri@hotmail.com>
2024-05-15 15:26:30 -0300

    update Users

--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -6,6 +6,7 @@ import { useDraggable } from "react-use-draggable-scroll"
 import { api } from "../api/api"
 import { User } from "../types/server/class"
 import { UserCard } from "../components/users/UserCard"
+import { NoFeaturedContent } from "../components/dashboard/NoFeaturedContent"
 
 interface UsersProps {}
 
@@ -75,27 +76,35 @@ export const Users: React.FC<UsersProps> = ({}) => {
                             flexDirection: "column",
                         }}
                     >
-                        <Grid container columns={3} spacing={2} sx={{ pb: "1vw" }}>
-                            {loading
-                                ? skeletonUserCards.map((_, index) => (
-                                      <>
-                                          <Grid key={index} item xs={1}>
-                                              <Paper sx={{ position: "relative" }}>
-                                                  <Box sx={{ flex: 1, justifyContent: "space-between", p: "0.7vw" }}>
-                                                      <Box sx={{ alignItems: "center", gap: "0.5vw" }}>
-                                                          <Skeleton variant="circular" sx={{ width: "4.5vw", height: "4.5vw" }} />
-                                                          <Box sx={{ flexDirection: "column", gap: "0.5vw" }}>
-                                                              <Skeleton variant="rectangular" sx={{ width: "8vw" }} />
-                                                              <Skeleton variant="rectangular" sx={{ width: "15vw" }} />
+                        {users.length === 0 ? (
+                            <>
+                                <NoFeaturedContent />
+                            </>
+                        ) : (
+                            <>
+                                <Grid container columns={3} spacing={2} sx={{ pb: "1vw" }}>
+                                    {loading
+                                        ? skeletonUserCards.map((_, index) => (
+                                              <>
+                                                  <Grid key={index} item xs={1}>
+                                                      <Paper sx={{ position: "relative" }}>
+                                                          <Box sx={{ flex: 1, justifyContent: "space-between", p: "0.7vw" }}>
+                                                              <Box sx={{ alignItems: "center", gap: "0.5vw" }}>
+                                                                  <Skeleton variant="circular" sx={{ width: "4.5vw", height: "4.5vw" }} />
+                                                                  <Box sx={{ flexDirection: "column", gap: "0.5vw" }}>
+                                                                      <Skeleton variant="rectangular" sx={{ width: "8vw" }} />
+                                                                      <Skeleton variant="rectangular" sx={{ width: "15vw" }} />
+                                                                  </Box>
+                                                              </Box>
                                                           </Box>
-                                                      </Box>
-                                                  </Box>
-                                              </Paper>
-                                          </Grid>
-                                      </>
-                                  ))
-                                : users.map((user) => <UserCard user={user} key={user.id} />)}
-                        </Grid>
+                                                      </Paper>
+                                                  </Grid>
+                                              </>
+                                          ))
+                                        : users.map((user) => <UserCard user={user} key={user.id} />)}
+                                </Grid>
+                            </>
+                        )}
                     </Box>
                 </Box>
             </Box>
