2d2e7b64ba6601fb0c845f775244b01226297d36
alfredo-petri <alfredo_petri@hotmail.com>
2024-05-15 08:30:31 -0300

    update placeholders

--- src/tools/placeholders.ts
+++ src/tools/placeholders.ts
@@ -1,8 +1,8 @@
 import cover from "../../public/placeholders/capa.webp"
 import square from "../../public/placeholders/midia_1-1.webp"
-import landscape from "../../assets/placeholders/midia_16-9.webp"
-import avatar from "../../assets/placeholders/perfil.webp"
-import video from "../../assets/placeholders/video.webp"
+import landscape from "../../public/placeholders/midia_16-9.webp"
+import avatar from "../../public/placeholders/perfil.webp"
+import video from "../../public/placeholders/video.webp"
 
 export default {
     cover,
