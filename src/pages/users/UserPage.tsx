ab511f6e6fc17530c050a2b9ac8246ec0cf7c7df
alfredo-petri <alfredo_petri@hotmail.com>
2024-05-23 16:32:27 -0300

    update first column UserPage

--- src/pages/users/UserPage.tsx
+++ src/pages/users/UserPage.tsx
@@ -1,29 +1,68 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import { Avatar, Box, Button, Grid, IconButton, Paper, TextField, Typography } from "@mui/material"
 import { useLocation } from "react-router-dom"
 import { HeaderInfo } from "../../components/header/HeaderInfo"
 import { User } from "../../types/server/class"
 import MoreVertIcon from "@mui/icons-material/MoreVert"
+import placeholders from "../../tools/placeholders"
+import { api } from "../../api/api"
 
 interface UserPageProps {}
 
 export const UserPage: React.FC<UserPageProps> = ({}) => {
     const location = useLocation()
     const [user, setuser] = useState(location.state.user as User)
-    console.log(user)
+    const creator = user.creator
+
+    const [loading, setloading] = useState(false)
+
+    const [statistic, setStatistic] = useState([])
+
+    const fetchStatistic = async () => {
+        if (loading) return
+        setloading(true)
+
+        try {
+            const response = await api.get("/creator/statistics", { params: { creator_id: creator!.id } })
+            setStatistic(response.data)
+        } catch (error) {
+            console.log(error)
+        } finally {
+            setTimeout(() => {
+                setloading(false)
+            }, 300)
+        }
+    }
+
+    useEffect(() => {
+        fetchStatistic()
+    }, [])
 
     return (
-        <Box sx={{ flexDirection: "column", gap: "1vw", width: "76vw", height: "74vh" }}>
-            <HeaderInfo title={`Usuário: ${user.name}`} refreshButton={false} exitButton={false} backButton />
-            <Box sx={{ gap: "1vw" }}>
-                <Box sx={{ height: 1, flexDirection: "column", justifyContent: "space-between" }}>
+        <Box sx={{ flexDirection: "column", gap: "1vw", width: "76vw", height: "71.6vh" }}>
+            <HeaderInfo title={`Informações do criador de conteúdo`} refreshButton={false} exitButton={false} backButton />
+            <Box sx={{ gap: "1vw", height: 1 }}>
+                <Box sx={{ height: 1, width: "25vw", maxWidth: "23.6vw", flexDirection: "column", justifyContent: "space-between", gap: "0.5vw" }}>
+                    <Typography variant="body1" component="p" sx={{ fontSize: "1.3rem" }}>
+                        Nome: {creator?.nickname}
+                    </Typography>
                     <Box sx={{ flexDirection: "column", gap: "1vw" }}>
                         <Paper sx={{ borderRadius: "1vw" }}>
-                            <Avatar src={user.cover || undefined} sx={{ width: "23.6vw", height: "13.27vw", borderRadius: "1vw" }} />
+                            <Avatar
+                                src={(creator ? creator.cover : user.cover) || placeholders.landscape}
+                                sx={{ width: "23.6vw", height: "13.27vw", borderRadius: "1vw" }}
+                            >
+                                <Avatar src={placeholders.landscape} />
+                            </Avatar>
                         </Paper>
-                        <Avatar src={user.image || undefined} sx={{ width: "6vw", height: "auto", marginTop: "-4vw" }} />
+                        <Avatar
+                            src={(creator ? creator.image : user.image) || placeholders.avatar}
+                            sx={{ width: "6vw", height: "6vw", marginTop: "-4vw" }}
+                        >
+                            <Avatar src={placeholders.avatar} />
+                        </Avatar>
                         <Box sx={{ marginLeft: "auto", marginTop: "-3.5vw", gap: "0.5vw", alignItems: "center" }}>
-                            <TextField InputProps={{ sx: { height: "1.7vw", width: "9vw" } }} sx={{}} select />
+                            <TextField InputProps={{ sx: { height: "1.7vw", width: "9vw" } }} sx={{}} />
                             <Button variant="contained" sx={{ height: "1.7vw", padding: 0, borderRadius: "3vw" }}>
                                 Salvar
                             </Button>
@@ -32,7 +71,7 @@ export const UserPage: React.FC<UserPageProps> = ({}) => {
                             </IconButton>
                         </Box>
                         <Box>
-                            <Typography sx={{ textAlign: "right" }}>{user.bio}</Typography>
+                            <Typography sx={{ textAlign: "right", maxWidth: "23.6vw", flex: 1 }}>{creator?.description}</Typography>
                         </Box>
                     </Box>
                     <Paper sx={{ flexDirection: "column", gap: "1vw", height: "6vw", marginTop: "auto" }}>
@@ -50,15 +89,13 @@ export const UserPage: React.FC<UserPageProps> = ({}) => {
                         </Box>
                     </Paper>
                 </Box>
-                <Grid container spacing={3} columns={user.creator ? 2 : 1} sx={{}}>
+                <Grid container spacing={3} columns={2} sx={{}}>
                     <Grid item xs={1}>
                         <Box sx={{ flex: 1, border: "1px solid red" }}></Box>
                     </Grid>
-                    {user.creator && (
-                        <Grid item xs={1}>
-                            <Box sx={{ flex: 1, border: "1px solid red" }}></Box>
-                        </Grid>
-                    )}
+                    <Grid item xs={1}>
+                        <Box sx={{ flex: 1, border: "1px solid red" }}></Box>
+                    </Grid>
                 </Grid>
             </Box>
         </Box>
