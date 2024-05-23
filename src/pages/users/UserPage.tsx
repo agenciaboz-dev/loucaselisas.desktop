06df3ee473c40eee175f9bbce4f9766994c92547
alfredo-petri <alfredo_petri@hotmail.com>
2024-05-23 10:23:14 -0300

    update UserPage

--- src/pages/users/UserPage.tsx
+++ src/pages/users/UserPage.tsx
@@ -6,8 +6,7 @@ interface UserPageProps {}
 
 export const UserPage: React.FC<UserPageProps> = ({}) => {
     const location = useLocation()
+    const [user, setuser] = useState(location.state.user)
 
-    const [user, setuser] = useState(location.state.data)
-
-    return <Box sx={{}}>{user.name}</Box>
+    return user ? <Box sx={{ flexDirection: "column", gap: "1vw" }}>{user.name}</Box> : null
 }
