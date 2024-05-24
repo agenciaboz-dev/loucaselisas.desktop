0feb84d724092885fdacc1477b57ac9d03e3b578
alfredo-petri <alfredo_petri@hotmail.com>
2024-05-24 14:04:43 -0300

    update fetch course path

--- src/hooks/useGetCourses.ts
+++ src/hooks/useGetCourses.ts
@@ -10,7 +10,7 @@ export const useGetCourses = () => {
         setLoading(true)
 
         try {
-            const response = await api.get("/course/admin")
+            const response = await api.get("/course")
             courses = response.data
         } catch (error) {
             console.error(error)
