b1af08301bec89b8affebddac7cf5067fbf5cb18
alfredo-petri <alfredo_petri@hotmail.com>
2024-05-24 09:01:40 -0300

    update StatisticsView

--- src/pages/users/StatisticsView.tsx
+++ src/pages/users/StatisticsView.tsx
@@ -2,18 +2,23 @@ import React from "react"
 import { Box, Typography } from "@mui/material"
 
 interface StatisticsViewProps {
-    statistic: number
+    statistic: number | undefined
     icon: React.ReactNode
     text: string
 }
 
 export const StatisticsView: React.FC<StatisticsViewProps> = ({ statistic, icon, text }) => {
     return (
-        <Box sx={{ flex: 1, border: "1px solid red", height: "3vw", flexDirection: "column", alignItems: "center" }}>
-            <Box sx={{ gap: "0.5vw" }}>
-                <Typography>{statistic}</Typography> {icon}
+        <Box sx={{ flex: 1, height: "3vw", flexDirection: "column", alignItems: "center" }}>
+            <Box sx={{ gap: "0.5vw", alignItems: "center" }}>
+                <Typography variant="body1" component="p" sx={{ fontSize: "1.1rem" }}>
+                    {statistic}
+                </Typography>{" "}
+                {icon}
             </Box>
-            <Typography>{text}</Typography>
+            <Typography variant="body1" component="p" sx={{ fontSize: "1.1rem", mt: "-0.3vw" }}>
+                {text}
+            </Typography>
         </Box>
     )
 }
