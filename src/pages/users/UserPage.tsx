622f4e489358a7446eb7f5f01ed9f426ad359b4f
alfredo-petri <alfredo_petri@hotmail.com>
2024-05-29 14:25:53 -0300

    UserPage

--- src/pages/users/UserPage.tsx
+++ src/pages/users/UserPage.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react"
-import { Avatar, Box, Button, Grid, MenuItem, Paper, Switch, TextField, Typography } from "@mui/material"
+import { Avatar, Box, Button, CircularProgress, Grid, MenuItem, Paper, Switch, TextField, Typography } from "@mui/material"
 import { useLocation } from "react-router-dom"
 import { HeaderInfo } from "../../components/header/HeaderInfo"
 import { User } from "../../types/server/class"
@@ -23,62 +23,43 @@ interface MessageItem {
 
 type Messages = MessageItem[]
 
-// type data = [...Course[], ...Lesson[]]
-
 export const UserPage: React.FC<UserPageProps> = ({}) => {
-    // const gridColumnStyle = {
-    //     height: "69.5vh",
-    //     flexDirection: "column",
-    //     padding: "0.1vw 0.2vw 0.7vw",
-    //     gap: "0.5vw",
-    //     overflow: "scroll",
-    // }
-
     const location = useLocation()
     const userId = location.state.userId as string | undefined
-    const [user, setuser] = useState(location.state.user as User | undefined)
+    const [user, setUser] = useState(location.state.user as User | undefined)
     const id = user ? user?.id : userId
 
     const [loading, setLoading] = useState(false)
 
     const [userTypes, setUserTypes] = useState<Role[]>([])
     const [messages, setMessages] = useState<Messages>([])
-    // const [courses, setCourses] = useState<Course[]>([])
-    // const [lessons, setLessons] = useState<Lesson[]>([])
-    // const [currentTab, setCurrentTab] = useState(1)
 
-    const required_message = "Campo obrigatório"
-    const validateSchema = Yup.object().shape({
-        role: Yup.number().min(1, required_message),
-    })
+    const [selectedRole, setSelectedRole] = useState<Role>()
 
-    const formik = useFormik<PartialUser>({
-        initialValues: { id: user!.id, role: user?.role.id },
-        onSubmit: async (values) => {
-            if (loading) return
-            setLoading(true)
+    const onSubmit = async (value: Role) => {
+        if (loading) return
+        setLoading(true)
 
-            try {
-                console.log(values)
-                const response = await api.patch("/user", values)
-                console.log(response.data)
-            } catch (error) {
-                console.log(error)
-            } finally {
-                setTimeout(() => {
-                    setLoading(false)
-                }, 300)
-            }
-        },
-        validationSchema: validateSchema,
-    })
+        try {
+            console.log(value)
+            const response = await api.patch("/user", { role: value, id: user?.id })
+            console.log(response.data)
+            setUser(response.data)
+        } catch (error) {
+            console.log(error)
+        } finally {
+            setTimeout(() => {
+                setLoading(false)
+            }, 300)
+        }
+    }
 
     const fetchUser = async () => {
         if (loading) return
         setLoading(true)
         try {
             const response = await api.get("/user", { params: { id: id } })
-            setuser(response.data)
+            setUser(response.data)
         } catch (error) {
             console.log(error)
         } finally {
@@ -119,36 +100,6 @@ export const UserPage: React.FC<UserPageProps> = ({}) => {
         }
     }
 
-    // const fetchCourses = async () => {
-    //     if (loading) return
-    //     setLoading(true)
-    //     try {
-    //         const response = await api.get("/course/owner", { params: { owner_id: creator?.id } })
-    //         setCourses(response.data)
-    //     } catch (error) {
-    //         console.log(error)
-    //     } finally {
-    //         setTimeout(() => {
-    //             setLoading(false)
-    //         }, 300)
-    //     }
-    // }
-
-    // const fetchLessons = async () => {
-    //     if (loading) return
-    //     setLoading(true)
-    //     try {
-    //         const response = await api.get("/creator/lessons", { params: { creator_id: creator?.id } })
-    //         setLessons(response.data)
-    //     } catch (error) {
-    //         console.log(error)
-    //     } finally {
-    //         setTimeout(() => {
-    //             setLoading(false)
-    //         }, 300)
-    //     }
-    // }
-
     useEffect(() => {
         fetchUser()
     }, [])
@@ -157,16 +108,9 @@ export const UserPage: React.FC<UserPageProps> = ({}) => {
         if (user) {
             fetchMessages()
             fetchUsersTypes()
+            setSelectedRole(user.role)
         }
     }, [user])
-
-    // useEffect(() => {
-    //     if (creator) {
-    //         fetchStatistic()
-    //         fetchCourses()
-    //         fetchLessons()
-    //     }
-    // }, [creator])
 
     return user ? (
         <Box sx={{ flexDirection: "column", gap: "1vw", width: "76vw", height: "71.6vh" }}>
@@ -192,27 +136,34 @@ export const UserPage: React.FC<UserPageProps> = ({}) => {
                             </Box>
                         </Box>
                         <Box sx={{ marginLeft: "auto", gap: "0.5vw", alignItems: "center", marginTop: "-3vw" }}>
-                            <form onSubmit={formik.handleSubmit}>
-                                <TextField //todo finalizar a função de seleção
-                                    name="role"
-                                    value={formik.values.role}
-                                    onChange={formik.handleChange}
-                                    InputProps={{ sx: { height: "1.7vw", width: "12.5vw" } }}
-                                    SelectProps={{ MenuProps: { MenuListProps: { sx: { width: 1 } } }, multiline: true }}
-                                    select
-                                    error={formik.touched.role && Boolean(formik.errors.role)}
-                                    helperText={formik.touched.role && formik.errors.role}
-                                >
-                                    {userTypes.map((type) => (
-                                        <MenuItem value={type.id} key={type.id}>
-                                            {type.name}
-                                        </MenuItem>
-                                    ))}
-                                </TextField>
-                                <Button type="submit" variant="contained" sx={{ height: "1.7vw", padding: 0, borderRadius: "3vw" }}>
-                                    Salvar
-                                </Button>
-                            </form>
+                            {selectedRole && (
+                                <>
+                                    <TextField
+                                        name="selectedRole"
+                                        value={selectedRole}
+                                        onChange={(e) => setSelectedRole(userTypes.find((item) => item.id.toString() == e.target.value))}
+                                        InputProps={{ sx: { height: "1.7vw", width: "12.5vw" } }}
+                                        SelectProps={{
+                                            MenuProps: { MenuListProps: { sx: { width: 1 } } },
+                                            renderValue: (selected: Role) => selected.name,
+                                        }}
+                                        select
+                                    >
+                                        {userTypes.map((type) => (
+                                            <MenuItem value={type.id} key={type.id}>
+                                                {type.name}
+                                            </MenuItem>
+                                        ))}
+                                    </TextField>
+                                    <Button
+                                        onClick={() => onSubmit(selectedRole)}
+                                        variant="contained"
+                                        sx={{ height: "1.7vw", padding: 0, borderRadius: "3vw" }}
+                                    >
+                                        {loading ? <CircularProgress /> : "Salvar"}
+                                    </Button>
+                                </>
+                            )}
                         </Box>
                         <Box>
                             <Typography
@@ -235,66 +186,7 @@ export const UserPage: React.FC<UserPageProps> = ({}) => {
                     </Box>
                     <Box sx={{ flexDirection: "column", gap: "1vw", height: "6vw", marginTop: "auto" }}></Box>
                 </Box>
-                {/* {creator ? (
-                    <Grid container spacing={3} columns={2} sx={{}}>
-                        <Grid item xs={1}>
-                            <ColumnTitle prop="Ultímos Comentários" />
-                            <Box sx={{ ...gridColumnStyle }}>
-                                {messages.map((item) => (
-                                    <MessageCard key={item.message.id} message={item.message} course={item.course} sx={{ width: "24.4vw" }} />
-                                ))}
-                            </Box>
-                        </Grid>
-                        <Grid item xs={1}>
-                            <Tabs value={currentTab} onChange={(_, value) => setCurrentTab(value)} variant="fullWidth">
-                                <Tab value={1} label="Cursos" />
-                                <Tab value={2} label="Lessons" />
-                            </Tabs>
-                            <Box
-                                sx={{
-                                    ...gridColumnStyle,
-                                    pt: "0.2vw",
-                                    pb: "1.6vw",
-                                }}
-                            >
-                                {currentTab === 1 &&
-                                    courses.map((course) => (
-                                        <DataCard
-                                            key={course.id}
-                                            course={course}
-                                            image={course.cover}
-                                            title={course.name}
-                                            description={course.description}
-                                            likes={course.likes}
-                                            downloads={course.downloads}
-                                            messages={course.chat?.messages}
-                                            views={course.views}
-                                            link={`/cursos/${slugify(course.name)}`}
-                                            routerParam={course}
-                                            sx={{ width: "24.4vw" }}
-                                        />
-                                    ))}
-                                {currentTab === 2 &&
-                                    lessons.map((lesson) => (
-                                        <DataCard
-                                            key={lesson.id}
-                                            lesson={lesson}
-                                            image={lesson.thumb || lesson.media.url}
-                                            title={lesson.name}
-                                            description={lesson.info}
-                                            likes={lesson.likes}
-                                            downloads={lesson.downloads}
-                                            views={lesson.views}
-                                            userName={lesson.course.name}
-                                            link={`/licoes/${slugify(lesson.name)}`}
-                                            routerParam={{ lesson }}
-                                            sx={{ width: "24.4vw" }}
-                                        />
-                                    ))}
-                            </Box>
-                        </Grid>
-                    </Grid>
-                ) : ( */}
+
                 <Box sx={{ flexDirection: "column", gap: "0.5vw" }}>
                     <ColumnTitle prop="Últimos comentários" sx={{ width: 1 }} />
                     <Grid container spacing={3} sx={{ width: "53vw" }}>
