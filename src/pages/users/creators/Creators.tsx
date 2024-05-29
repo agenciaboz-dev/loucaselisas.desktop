71b55882e699f21fe3e0137cef6d037c5f86e4d6
alfredo-petri <alfredo_petri@hotmail.com>
2024-05-29 07:26:24 -0300

    create Creators

---
+++ src/pages/users/creators/Creators.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+import { Box } from "@mui/material"
+
+interface CreatorsProps {}
+
+export const Creators: React.FC<CreatorsProps> = ({}) => {
+    return <Box sx={{}}></Box>
+}
