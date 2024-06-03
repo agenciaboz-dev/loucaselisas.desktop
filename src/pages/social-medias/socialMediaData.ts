6af32869351c7242dcc0fe8a12616cc19d6bd6ae
alfredo-petri <alfredo_petri@hotmail.com>
2024-06-03 14:31:47 -0300

    create SocialMediaData

---
+++ src/pages/social-medias/socialMediaData.ts
@@ -0,0 +1,9 @@
+import WhatsAppIcon from "@mui/icons-material/WhatsApp"
+import InstagramIcon from "@mui/icons-material/Instagram"
+import { FaFacebookF } from "react-icons/fa"
+
+export const socialMediaData = [
+    { Icon: WhatsAppIcon, link: "https://wa.me/5548999401049" },
+    { Icon: InstagramIcon, link: "https://www.instagram.com/loucaselisas/" },
+    { ReactIcon: FaFacebookF, link: "https://www.facebook.com/LoucasLisas/" },
+]
