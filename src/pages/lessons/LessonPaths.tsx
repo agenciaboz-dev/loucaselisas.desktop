85e83020b1f21b8743f9c0c6f698b167780b9d94
alfredo-petri <alfredo_petri@hotmail.com>
2024-06-05 14:37:44 -0300

    update LessonPaths

--- src/pages/lessons/LessonPaths.tsx
+++ src/pages/lessons/LessonPaths.tsx
@@ -1,15 +1,10 @@
-import React, { useEffect, useMemo, useState } from "react"
+import React, { useEffect, useState } from "react"
 import { Lesson } from "../../types/server/class/Course/Lesson"
 import { slugify } from "../../tools/urlMask"
 import { DataCard } from "../../components/courses/DataCard"
-import { Paths } from "../../types/paths"
 import { User } from "../../types/server/class"
 import { api } from "../../api/api"
-import { useNavigate } from "react-router-dom"
-import VisibilityOutlined from "@mui/icons-material/VisibilityOutlined"
-import ChatOutlinedIcon from "@mui/icons-material/ChatOutlined"
 import { Course } from "../../types/server/class/Course"
-import { Box } from "@mui/material"
 import { useGetPaths } from "../../hooks/useGetPaths"
 
 interface LessonPathsProps {
@@ -17,11 +12,9 @@ interface LessonPathsProps {
 }
 
 export const LessonPaths: React.FC<LessonPathsProps> = ({ lesson }) => {
-    const navigate = useNavigate()
     const [course, setCourse] = useState<Course>()
     const [user, setUser] = useState<User>()
-    const [creator, setCreator] = useState(user?.creator)
-    const { allPaths } = useGetPaths(user, course, lesson)
+    const { allPaths } = useGetPaths({ user, course, lesson })
 
     const fetchCourse = async () => {
         try {
@@ -50,18 +43,8 @@ export const LessonPaths: React.FC<LessonPathsProps> = ({ lesson }) => {
     }, [course])
 
     useEffect(() => {
-        if (user) setCreator(user.creator)
-    }, [user])
-
-    useEffect(() => {
         fetchUser()
     }, [])
-
-    useEffect(() => {
-        if (user) {
-            setCreator(user.creator)
-        }
-    }, [user])
 
     return (
         <DataCard
