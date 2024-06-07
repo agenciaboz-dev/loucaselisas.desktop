d2a99696baf7bdfb28bc80d860c144e30b5e1df8
alfredo-petri <alfredo_petri@hotmail.com>
2024-06-07 09:59:21 -0300

    update MessageVideo

--- src/components/groups/MessageVideo.tsx
+++ src/components/groups/MessageVideo.tsx
@@ -1,21 +1,37 @@
 import React from "react"
 import { Avatar, Box, Typography } from "@mui/material"
+import { Message } from "../../types/server/class/Chat/Message"
+import placeholders from "../../tools/placeholders"
+import { Lesson } from "../../types/server/class/Course/Lesson"
+import moment from "moment"
+import "moment-duration-format"
 
-interface MessageVideoProps {}
+interface MessageVideoProps {
+    message: Message
+    lesson: Lesson
+}
 
-export const MessageVideo: React.FC<MessageVideoProps> = ({}) => {
+export const MessageVideo: React.FC<MessageVideoProps> = ({ message, lesson }) => {
     return (
-        <Box>
+        <Box sx={{ gap: "1vw" }}>
             <Avatar
-                src={"/placeholders/video.webp"}
+                src={lesson.media.url || placeholders.video}
                 sx={{
                     borderRadius: "0.5vw",
                     width: "3vw",
                     height: "3vw",
                 }}
-            />
-            <Box></Box>
-            <Typography></Typography>
+            >
+                <Avatar src={placeholders.video} />
+            </Avatar>
+            <Box sx={{ flexDirection: "column" }}>
+                <Typography sx={{ maxWidth: "15vw" }}>{lesson.name}</Typography>
+                <Typography sx={{ maxWidth: "15vw" }}>{lesson.info}</Typography>
+            </Box>
+            <Typography>
+                {/* @ts-ignore */}
+                {moment.duration(message.video_timestamp).format("mm:ss", { trim: false })}
+            </Typography>
         </Box>
     )
 }
