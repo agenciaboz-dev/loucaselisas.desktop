b2a4b421e2df72c4cb6f7230aeaec996bfc0aea7
alfredo-petri <alfredo_petri@hotmail.com>
2024-06-07 09:59:11 -0300

    update messageImage

--- src/components/groups/MessageImage.tsx
+++ src/components/groups/MessageImage.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { Avatar, Box, MenuItem } from "@mui/material"
 import { Message } from "../../types/server/class/Chat/Message"
+import placeholders from "../../tools/placeholders"
 
 interface MessageImageProps {
     message: Message
@@ -11,11 +12,13 @@ export const MessageImage: React.FC<MessageImageProps> = ({ message, setIsOpen }
     return (
         <MenuItem sx={{ padding: 0 }}>
             <Avatar
-                src={message.media?.url}
+                src={message.media?.url || placeholders.landscape}
                 variant="rounded"
                 sx={{ width: "15vw", height: "8.43vw", borderRadius: "0.9vw" }}
                 onClick={() => setIsOpen(true)}
-            />
+            >
+                <Avatar src={placeholders.landscape} sx={{ width: "15vw", height: "8.43vw", borderRadius: "0.9vw" }} />
+            </Avatar>
         </MenuItem>
     )
 }
