1a795ac5fbc9b1ada9305434216ce36815d5f261
alfredo-petri <alfredo_petri@hotmail.com>
2024-06-07 14:47:35 -0300

    update MessageVideo

--- src/components/groups/MessageVideo.tsx
+++ src/components/groups/MessageVideo.tsx
@@ -1,10 +1,12 @@
 import React from "react"
-import { Avatar, Box, Typography } from "@mui/material"
+import { Avatar, Box, Paper, Typography } from "@mui/material"
 import { Message } from "../../types/server/class/Chat/Message"
 import placeholders from "../../tools/placeholders"
 import { Lesson } from "../../types/server/class/Course/Lesson"
 import moment from "moment"
-import "moment-duration-format"
+import dayjs from "dayjs"
+import duration from "dayjs/plugin/duration"
+dayjs.extend(duration)
 
 interface MessageVideoProps {
     message: Message
@@ -12,26 +14,50 @@ interface MessageVideoProps {
 }
 
 export const MessageVideo: React.FC<MessageVideoProps> = ({ message, lesson }) => {
+    const formattedDuration = dayjs.duration(Number(message.video_timestamp)).format("mm:ss")
+
     return (
-        <Box sx={{ gap: "1vw" }}>
-            <Avatar
-                src={lesson.media.url || placeholders.video}
-                sx={{
-                    borderRadius: "0.5vw",
-                    width: "3vw",
-                    height: "3vw",
-                }}
-            >
-                <Avatar src={placeholders.video} />
-            </Avatar>
-            <Box sx={{ flexDirection: "column" }}>
-                <Typography sx={{ maxWidth: "15vw" }}>{lesson.name}</Typography>
-                <Typography sx={{ maxWidth: "15vw" }}>{lesson.info}</Typography>
+        <Box sx={{ gap: "1vw", alignItems: "flex-start", justifyContent: "space-between" }}>
+            <Box sx={{ gap: "1vw" }}>
+                <Avatar
+                    src={lesson.media.url || placeholders.video}
+                    sx={{
+                        borderRadius: "0.5vw",
+                        width: "3.75vw",
+                        height: "3.75vw",
+                    }}
+                >
+                    <Avatar
+                        src={placeholders.video}
+                        sx={{
+                            borderRadius: "0.5vw",
+                            width: "3.75vw",
+                            height: "3.75vw",
+                        }}
+                    />
+                </Avatar>
+                <Box sx={{ flexDirection: "column" }}>
+                    <Typography variant="body1" component="p" sx={{ maxWidth: "24vw", fontWeight: 700 }}>
+                        {lesson.name}
+                    </Typography>
+                    <Typography
+                        variant="body1"
+                        component="p"
+                        sx={{
+                            maxWidth: "24vw",
+                            overflow: "hidden",
+                            textOverflow: "ellipsis",
+                            whiteSpace: "normal",
+                            display: "-webkit-box",
+                            WebkitLineClamp: 2,
+                            WebkitBoxOrient: "vertical",
+                        }}
+                    >
+                        {lesson.info}
+                    </Typography>
+                </Box>
             </Box>
-            <Typography>
-                {/* @ts-ignore */}
-                {moment.duration(message.video_timestamp).format("mm:ss", { trim: false })}
-            </Typography>
+            <Typography>{formattedDuration}</Typography>
         </Box>
     )
 }
