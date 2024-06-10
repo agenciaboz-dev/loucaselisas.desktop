1af74b7e09d838788c97fdb871a04446d4dcd578
alfredo-petri <alfredo_petri@hotmail.com>
2024-06-10 13:30:15 -0300

    update useGetPaths

--- src/hooks/useGetPaths.tsx
+++ src/hooks/useGetPaths.tsx
@@ -6,19 +6,39 @@ import { Course } from "../types/server/class/Course"
 import { useNavigate } from "react-router-dom"
 import VisibilityOutlined from "@mui/icons-material/VisibilityOutlined"
 import ChatOutlinedIcon from "@mui/icons-material/ChatOutlined"
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline"
 import { Lesson } from "../types/server/class/Course/Lesson"
+import { Message } from "../types/server/class/Chat/Message"
+import { api } from "../api/api"
+
+type Data = { messages: Message[]; chat_id: string }
 
 interface Options {
     user?: User | undefined
     course?: Course | undefined
     lesson?: Lesson | undefined
-    messageId?: string | undefined
+    message: Message
 }
 
 export const useGetPaths = (options: Options) => {
-    const { course, lesson, user, messageId } = options
+    const { course, lesson, user, message } = options
     const navigate = useNavigate()
     const creator = user?.creator
+    const messageID = message.id
+
+    const deleteMessage = async () => {
+        const data: Data = { messages: [message], chat_id: message.chat_id }
+
+        console.log(data)
+
+        try {
+            const response = await api.delete("/chat/delete_message", { data })
+            const thisMessage = response.data
+            console.log(thisMessage)
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
     const coursePaths: Paths = useMemo(
         () =>
@@ -92,11 +112,11 @@ export const useGetPaths = (options: Options) => {
             course
                 ? [
                       {
-                          link: `/grupos?id=${course.id}&messageId=${messageId}`,
+                          link: `/grupos?id=${course.id}&messageId=${message.id}`,
                           title: "Ver mensagem",
                           icon: <VisibilityOutlined />,
                           id: course.id,
-                          onClick: () => navigate(`/grupos?id=${course.id}&messageId=${messageId}`),
+                          onClick: () => navigate(`/grupos?id=${course.id}&messageId=${message.id}`),
                       },
                       {
                           link: `/grupos?id=${course.id}`,
@@ -104,6 +124,13 @@ export const useGetPaths = (options: Options) => {
                           icon: <VisibilityOutlined />,
                           id: course.id,
                           onClick: () => navigate(`/grupos?id=${course.id}`),
+                      },
+                      {
+                          link: messageID,
+                          title: "Deletar Mensagem",
+                          icon: <DeleteOutlineIcon />,
+                          id: messageID,
+                          onClick: deleteMessage,
                       },
                   ]
                 : [],
