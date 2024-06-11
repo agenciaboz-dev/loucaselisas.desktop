4e984b2f32aa63e87054429b9365db8781373ee2
alfredo-petri <alfredo_petri@hotmail.com>
2024-06-11 15:21:39 -0300

    update CoursePaths

--- src/pages/courses/CoursePaths.tsx
+++ src/pages/courses/CoursePaths.tsx
@@ -5,6 +5,7 @@ import { Course } from "../../types/server/class/Course"
 import { slugify } from "../../tools/urlMask"
 import { DataCard } from "../../components/courses/DataCard"
 import { useGetPaths } from "../../hooks/useGetPaths"
+import { ReproveModal } from "../../components/aprove/ReproveModal"
 
 interface CoursePathsProps {
     course: Course
@@ -12,6 +13,7 @@ interface CoursePathsProps {
 
 export const CoursePaths: React.FC<CoursePathsProps> = ({ course }) => {
     const [user, setUser] = useState<User>()
+    const [openModal, setOpenModal] = useState<boolean>(false)
 
     const fetchUser = async () => {
         try {
@@ -22,27 +24,36 @@ export const CoursePaths: React.FC<CoursePathsProps> = ({ course }) => {
         }
     }
 
-    const { coursePaths } = useGetPaths({ user, course })
+    const { coursePaths, reproveCourse } = useGetPaths({ user, course, setOpenModal })
 
     useEffect(() => {
         fetchUser()
     }, [])
 
     return (
-        <DataCard
-            key={course.id}
-            paths={coursePaths}
-            course={course}
-            image={course.cover}
-            title={course.name}
-            description={course.description}
-            likes={course.likes}
-            downloads={course.downloads}
-            messages={course.chat?.messages}
-            views={course.views}
-            userName={course.owner.user.username}
-            link={`/cursos/${slugify(course.name)}?id=${course.id}`}
-            routerParam={course}
-        />
+        <>
+            <ReproveModal
+                name={course.name}
+                onConfirm={reproveCourse}
+                type="course"
+                openReproveModal={openModal}
+                setOpenReproveModal={setOpenModal}
+            />
+            <DataCard
+                key={course.id}
+                paths={coursePaths}
+                course={course}
+                image={course.cover}
+                title={course.name}
+                description={course.description}
+                likes={course.likes}
+                downloads={course.downloads}
+                messages={course.chat?.messages}
+                views={course.views}
+                userName={course.owner.user.username}
+                link={`/cursos/${slugify(course.name)}?id=${course.id}`}
+                routerParam={course}
+            />
+        </>
     )
 }
