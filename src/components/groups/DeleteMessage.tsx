da7da956f6494adf11b93aba83c7a1dc017c439b
alfredo-petri <alfredo_petri@hotmail.com>
2024-06-12 09:48:42 -0300

    update DeleteMessaga

--- src/components/groups/DeleteMessage.tsx
+++ src/components/groups/DeleteMessage.tsx
@@ -1,13 +1,16 @@
 import React from "react"
 import { Box, IconButton, Paper } from "@mui/material"
 import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline"
+import { api } from "../../api/api"
 
-interface DeleteMessageProps {}
+interface DeleteMessageProps {
+    deleteMessage: () => void
+}
 
-export const DeleteMessage: React.FC<DeleteMessageProps> = ({}) => {
+export const DeleteMessage: React.FC<DeleteMessageProps> = ({ deleteMessage }) => {
     return (
         <Paper sx={{ borderRadius: "2vw" }}>
-            <IconButton>
+            <IconButton onClick={() => deleteMessage()}>
                 <DeleteOutlineIcon />
             </IconButton>
         </Paper>
