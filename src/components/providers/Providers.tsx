ef717646902164c7a1f99dce2af8ea9bd291d2e1
alfredo-petri <alfredo_petri@hotmail.com>
2024-06-12 13:21:56 -0300

    update Providers

--- src/components/providers/Providers.tsx
+++ src/components/providers/Providers.tsx
@@ -1,21 +1,23 @@
-import { ThemeProvider } from '@mui/material'
-import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
-import { usemuiTheme } from '../../hooks/useMuiTheme'
-import { UserProvider } from "../../contexts/userContext"
+import { ThemeProvider } from "@mui/material"
+import React from "react"
+import { BrowserRouter } from "react-router-dom"
+import { usemuiTheme } from "../../hooks/useMuiTheme"
+import { UserProvider } from "../../contexts/UserContext"
+import { TimeInstantProvider } from "../../contexts/TimeInstantContext"
 
 interface ProvidersProps {
     children?: React.ReactNode
 }
 
 export const Providers: React.FC<ProvidersProps> = ({ children }) => {
-
     const theme = usemuiTheme()
-    
+
     return (
         <BrowserRouter>
             <ThemeProvider theme={theme}>
-                <UserProvider>{children}</UserProvider>
+                <UserProvider>
+                    <TimeInstantProvider>{children}</TimeInstantProvider>
+                </UserProvider>
             </ThemeProvider>
         </BrowserRouter>
     )
